f54564865adafd7482d598bb19bf81b043dad899
Mayank Mishra <32954280+mayank31398@users.noreply.github.com>
2024-06-26 11:06:09 -0400

    Naive vec add (#2)

--- kernel_hyperdrive/__init__.py
+++ kernel_hyperdrive/__init__.py
@@ -1,9 +1,11 @@
 from .utils import compile_helpers
 from .vector_addition import (
     VectorAddition_CUDA,
+    VectorAddition_Naive,
     VectorAddition_PyTorch,
     VectorAddition_Triton,
     vector_addition_cuda,
+    vector_addition_naive,
     vector_addition_pytorch,
     vector_addition_triton,
 )
