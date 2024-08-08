6b6f9a4279dbf1b480487e180ca6cad6d607ce2a
Mayank Mishra <mayank31398@gmail.com>
2024-08-08 13:47:23 -0400

    some cleanup

--- kernel_hyperdrive/utils.py
+++ kernel_hyperdrive/utils.py
@@ -7,8 +7,8 @@ def compile_helpers() -> None:
     load_cpp_extension(
         "khd",
         sources=[
-            os.path.join(os.path.dirname(__file__), "vector_addition/cuda_kernel/vector_addition.cpp"),
-            os.path.join(os.path.dirname(__file__), "vector_addition/cuda_kernel/vector_addition.cu"),
+            os.path.join(os.path.dirname(__file__), "vector_addition/cuda_implementation/vector_addition.cpp"),
+            os.path.join(os.path.dirname(__file__), "vector_addition/cuda_implementation/vector_addition.cu"),
         ],
         with_cuda=True,
         extra_cflags=["-O3", "-Wall", "-shared", "-fPIC", "-fdiagnostics-color"],
