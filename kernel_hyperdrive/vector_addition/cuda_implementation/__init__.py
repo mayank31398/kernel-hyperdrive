f56b9af2623cf1f58734cd769ffc80f5d604a8a2
Mayank Mishra <mayank31398@gmail.com>
2024-08-10 02:25:06 -0400

    add op

--- kernel_hyperdrive/vector_addition/cuda_implementation/__init__.py
+++ kernel_hyperdrive/vector_addition/cuda_implementation/__init__.py
@@ -14,8 +14,16 @@ class _VectorAddition_CUDA(torch.autograd.Function):
         return output_grad, output_grad
 
 
+# this registers the kernel with PyTorch to make it work with torch.compile
+@torch.library.custom_op("khd::vector_addition_cuda", mutates_args=())
 def vector_addition_cuda(x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
     return _VectorAddition_CUDA.apply(x, y)
+
+
+# this tells torch.compile the output shape given the input shape
+@vector_addition_cuda.register_fake
+def _(x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
+    return torch.empty_like(x)
 
 
 class VectorAddition_CUDA(nn.Module):
