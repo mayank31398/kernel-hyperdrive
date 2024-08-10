9bac6b67ce8b17cbf0fbb9d6d0e6025584220468
Mayank Mishra <mayank31398@gmail.com>
2024-08-10 05:46:10 -0400

    drop compile test

--- tests/vector_addition_test.py
+++ tests/vector_addition_test.py
@@ -16,6 +16,7 @@ class VectorAdditionTest(TestCommons):
     )
     def test_vector_addition_cuda(self, size: int, device: torch.device, dtype: torch.dtype) -> None:
         self._test_vector_addition(size, device, dtype, vector_addition_cuda)
+        self._test_vector_addition(size, device, dtype, torch.compile(vector_addition_cuda))
 
     @parameterized.expand(
         TestCommons.make_args_matrix(
@@ -24,6 +25,7 @@ class VectorAdditionTest(TestCommons):
     )
     def test_vector_addition_triton(self, size: int, device: torch.device, dtype: torch.dtype) -> None:
         self._test_vector_addition(size, device, dtype, vector_addition_triton)
+        self._test_vector_addition(size, device, dtype, torch.compile(vector_addition_triton))
 
     def _test_vector_addition(self, size: int, device: torch.device, dtype: torch.dtype, function: Callable) -> None:
         x = torch.randn(size, device=device, dtype=dtype)
