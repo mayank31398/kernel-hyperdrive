d43d2f760702ae1be96fb938c9d44e064c5391c7
Mayank Mishra <mayank31398@gmail.com>
2024-08-15 18:06:52 -0400

    kernels

--- setup.py
+++ setup.py
@@ -1,14 +1,10 @@
-import yaml
 from setuptools import find_packages, setup
 
 
 VERSION = "0.0.0.dev"
 
-kernel_registry: dict = yaml.load(open("khd/kernel_registry.yml", "r"))
-all_cuda_sources = list(kernel_registry.values())
-
 setup(
-    name="kernels",
+    name="kernel-hyperdrive",
     version=VERSION,
     author="Mayank Mishra",
     author_email="mayank31398@ibm.com",
