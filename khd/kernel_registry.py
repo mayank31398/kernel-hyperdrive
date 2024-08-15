6899c0cdd6d5ed836727f8e10b678a351ee1fa3f
Mayank Mishra <mayank31398@gmail.com>
2024-08-15 18:04:03 -0400

    kernels

--- khd/kernel_registry.py
+++ khd/kernel_registry.py
@@ -1,16 +1,43 @@
 import importlib
 from typing import Callable
 
+import yaml
+from torch.utils.cpp_extension import load as load_cpp_extension
 
-_COMPILED_KERNEL_REGISTRY = {}
+
 _MODULE_NAME = "khd_cuda_kernels"
+_CUDA_KERNEL_MODULE = None
+_CUDA_KERNEL_SOURCES = []
 
 
-def get_kernel(name: str) -> Callable:
-    global _COMPILED_KERNEL_REGISTRY, _MODULE_NAME
+class KernelRegistry:
+    def __init__(self) -> None:
+        global _CUDA_KERNEL_MODULE, _CUDA_KERNEL_SOURCES
 
-    if name not in _COMPILED_KERNEL_REGISTRY:
-        cuda_kernel_module = importlib.import_module(_MODULE_NAME)
-        _COMPILED_KERNEL_REGISTRY[name] = getattr(cuda_kernel_module, name)
+        registry: dict = yaml.load(open("kernel_registry.yml", "r"))
 
-    return _COMPILED_KERNEL_REGISTRY[name]
+        functions = []
+        for module in registry:
+            functions.extend(module["functions"])
+
+        assert len(functions) == len(set(functions)), "function names are not unique"
+
+        for module in registry:
+            _CUDA_KERNEL_SOURCES.extend(module["sources"])
+
+        _CUDA_KERNEL_MODULE = load_cpp_extension(
+            _MODULE_NAME,
+            sources=_CUDA_KERNEL_SOURCES,
+            with_cuda=True,
+            extra_cflags=["-O3", "-Wall", "-shared", "-fPIC", "-fdiagnostics-color"],
+            build_directory="build",
+            verbose=True,
+        )
+
+    def get_sources(self) -> list[str]:
+        global _CUDA_KERNEL_SOURCES
+        return _CUDA_KERNEL_SOURCES
+
+    def get_kernel(self, name: str) -> Callable:
+        global _CUDA_KERNEL_MODULE
+        return getattr(_CUDA_KERNEL_MODULE, name)
