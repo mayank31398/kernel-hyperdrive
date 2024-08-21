1fa0e5c73bafdd23498aa1f10029bf6277ba3e11
Mayank Mishra <32954280+mayank31398@users.noreply.github.com>
2024-08-21 00:49:13 -0400

    use LLVM clang-format (#8)

--- khd/vector_addition/cuda_implementation/kernels.cu
+++ khd/vector_addition/cuda_implementation/kernels.cu
@@ -3,35 +3,42 @@
 #include <torch/extension.h>
 
 template <typename scalar_t>
-__global__ void vector_addition_forward_kernel(const scalar_t* x,
-                                               const scalar_t* y,
-                                               scalar_t* output,
-                                               const int num_elements)
-{
-    int index = blockIdx.x * blockDim.x + threadIdx.x;
-    if (index < num_elements) output[index] = x[index] + y[index];
+__global__ void vector_addition_forward_kernel(const scalar_t *x,
+                                               const scalar_t *y,
+                                               scalar_t *output,
+                                               const int num_elements) {
+  int index = blockIdx.x * blockDim.x + threadIdx.x;
+  if (index < num_elements)
+    output[index] = x[index] + y[index];
 }
 
-torch::Tensor vector_addition_forward_kernel_launcher(torch::Tensor x, torch::Tensor y, const int BLOCK_SIZE)
-{
-    int num_elements = x.numel();
-    torch::Tensor output = torch::empty_like(x);
+torch::Tensor vector_addition_forward_kernel_launcher(torch::Tensor x,
+                                                      torch::Tensor y,
+                                                      const int BLOCK_SIZE) {
+  int num_elements = x.numel();
+  torch::Tensor output = torch::empty_like(x);
 
-    int blocks = (int)ceil((float)num_elements / BLOCK_SIZE);
+  int blocks = (int)ceil((float)num_elements / BLOCK_SIZE);
 
-    if (at::isReducedFloatingType(x.scalar_type())) {
-        AT_DISPATCH_REDUCED_FLOATING_TYPES(
-            x.scalar_type(), "vector_addition_forward_kernel", ([&] {
-                vector_addition_forward_kernel<scalar_t><<<blocks, BLOCK_SIZE>>>(
-                    x.data<scalar_t>(), y.data<scalar_t>(), output.data<scalar_t>(), num_elements);
-            }));
-    } else {
-        AT_DISPATCH_FLOATING_TYPES(
-            x.scalar_type(), "vector_addition_forward_kernel", ([&] {
-                vector_addition_forward_kernel<scalar_t><<<blocks, BLOCK_SIZE>>>(
-                    x.data<scalar_t>(), y.data<scalar_t>(), output.data<scalar_t>(), num_elements);
-            }));
-    }
+  if (at::isReducedFloatingType(x.scalar_type())) {
+    AT_DISPATCH_REDUCED_FLOATING_TYPES(
+        x.scalar_type(), "vector_addition_forward_kernel", ([&] {
+          vector_addition_forward_kernel<scalar_t>
+              <<<blocks, BLOCK_SIZE>>>(x.data<scalar_t>(),
+                                       y.data<scalar_t>(),
+                                       output.data<scalar_t>(),
+                                       num_elements);
+        }));
+  } else {
+    AT_DISPATCH_FLOATING_TYPES(
+        x.scalar_type(), "vector_addition_forward_kernel", ([&] {
+          vector_addition_forward_kernel<scalar_t>
+              <<<blocks, BLOCK_SIZE>>>(x.data<scalar_t>(),
+                                       y.data<scalar_t>(),
+                                       output.data<scalar_t>(),
+                                       num_elements);
+        }));
+  }
 
-    return output;
+  return output;
 }
