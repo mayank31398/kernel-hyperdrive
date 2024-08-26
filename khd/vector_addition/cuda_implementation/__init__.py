fd9e9bd4db525bb270bb75f55b182909ca54eff1
Mayank Mishra <mayank31398@gmail.com>
2024-08-26 17:31:18 -0400

    last block

--- khd/vector_addition/cuda_implementation/__init__.py
+++ khd/vector_addition/cuda_implementation/__init__.py
@@ -4,19 +4,13 @@ from ...kernel_registry import KernelRegistry
 
 
 _KERNEL_NAME = "vector_addition_forward"
-_cuda_kernel = KernelRegistry.get_kernel(_KERNEL_NAME)
 
 
-# this registers the kernel with PyTorch to make it work with torch.compile
-@torch.library.custom_op(f"khd::{_KERNEL_NAME}", mutates_args=())
 def _vector_addition_forward_cuda_kernel(x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
-    return _cuda_kernel(x, y)
+    if not hasattr(_vector_addition_forward_cuda_kernel, "_kernel"):
+        _vector_addition_forward_cuda_kernel._kernel = KernelRegistry.get_kernel(_KERNEL_NAME)
 
-
-# this tells torch.compile the output shape given the input shape
-@_vector_addition_forward_cuda_kernel.register_fake
-def _(x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
-    return torch.empty_like(x)
+    return _vector_addition_forward_cuda_kernel._kernel(x, y)
 
 
 class _VectorAddition_CUDA(torch.autograd.Function):
