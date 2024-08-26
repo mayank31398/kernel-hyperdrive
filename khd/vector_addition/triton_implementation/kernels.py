77a9aafb43fe2b5d2da1c9ec974ad3e455a95566
Mayank Mishra <mayank31398@gmail.com>
2024-08-26 02:12:53 -0400

    unguarded

--- khd/vector_addition/triton_implementation/kernels.py
+++ khd/vector_addition/triton_implementation/kernels.py
@@ -5,11 +5,13 @@ import triton.language as tl
 @triton.jit
 def vector_addition_forward_triton_kernel(x_ptr, y_ptr, output_ptr, num_elements, BLOCK_SIZE: tl.constexpr):
     pid = tl.program_id(axis=0)
+    num_blocks = tl.num_programs(axis=0)
 
     block_start = pid * BLOCK_SIZE
     block_indices = block_start + tl.arange(0, BLOCK_SIZE)
 
-    mask = block_indices < num_elements
+    is_last_block = pid == num_blocks - 1
+    mask = block_indices < num_elements if is_last_block else None
 
     x = tl.load(x_ptr + block_indices, mask=mask)
     y = tl.load(y_ptr + block_indices, mask=mask)
