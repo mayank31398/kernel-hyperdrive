c28dd3fabc1982770c76462d38c3ce500c30c9e1
Mayank Mishra <mayank31398@gmail.com>
2024-08-26 02:03:35 -0400

    revert clang-format

--- khd/vector_addition/cuda_implementation/ops.cpp
+++ khd/vector_addition/cuda_implementation/ops.cpp
@@ -1,29 +1,24 @@
 #include <torch/extension.h>
 
 // CUDA kernel declarations
-torch::Tensor vector_addition_forward_kernel_launcher(torch::Tensor x,
-                                                      torch::Tensor y,
-                                                      const int BLOCK_SIZE);
+torch::Tensor vector_addition_forward_kernel_launcher(torch::Tensor x, torch::Tensor y, const int BLOCK_SIZE);
 
 torch::Tensor vector_addition_forward(torch::Tensor x, torch::Tensor y) {
-  TORCH_CHECK(x.device().is_cuda(), "tensor x is not on GPU")
-  TORCH_CHECK(y.device().is_cuda(), "tensor y is not on GPU")
+    TORCH_CHECK(x.device().is_cuda(), "tensor x is not on GPU")
+    TORCH_CHECK(y.device().is_cuda(), "tensor y is not on GPU")
 
-  TORCH_CHECK(x.is_contiguous(), "tensor x is not a contiguous")
-  TORCH_CHECK(y.is_contiguous(), "tensor y is not a contiguous")
+    TORCH_CHECK(x.is_contiguous(), "tensor x is not a contiguous")
+    TORCH_CHECK(y.is_contiguous(), "tensor y is not a contiguous")
 
-  TORCH_CHECK(x.dim() == 1, "tensor x should be 1 dimensional")
-  TORCH_CHECK(y.dim() == 1, "tensor y should be 1 dimensional")
+    TORCH_CHECK(x.dim() == 1, "tensor x should be 1 dimensional")
+    TORCH_CHECK(y.dim() == 1, "tensor y should be 1 dimensional")
 
-  TORCH_CHECK(x.numel() == y.numel(),
-              "both tensors should have same number of elements");
-  TORCH_CHECK(x.type() == y.type(), "both tensors should have same dtype");
+    TORCH_CHECK(x.numel() == y.numel(), "both tensors should have same number of elements");
+    TORCH_CHECK(x.type() == y.type(), "both tensors should have same dtype");
 
-  return vector_addition_forward_kernel_launcher(x, y, 1024);
+    return vector_addition_forward_kernel_launcher(x, y, 1024);
 }
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-  m.def("vector_addition_forward",
-        &vector_addition_forward,
-        "Vector addition forward (CUDA)");
+    m.def("vector_addition_forward", &vector_addition_forward, "Vector addition forward (CUDA)");
 }
