cb3abd981e12842e151a48eec9a2e284f83309ab
Mayank Mishra <32954280+mayank31398@users.noreply.github.com>
2024-08-30 10:26:26 -0400

    some reformatting (#10)

--- khd/vector_addition/cuda_implementation/kernels.cu
+++ khd/vector_addition/cuda_implementation/kernels.cu
@@ -8,29 +8,16 @@ __global__ void vector_addition_forward_kernel(const scalar_t *x,
                                                scalar_t *output,
                                                const int num_elements) {
     int index = blockIdx.x * blockDim.x + threadIdx.x;
-    if (index < num_elements)
+    if (index < num_elements) {
         output[index] = x[index] + y[index];
+    }
 }
 
-torch::Tensor vector_addition_forward_kernel_launcher(torch::Tensor x, torch::Tensor y, const int BLOCK_SIZE) {
-    int num_elements = x.numel();
-    torch::Tensor output = torch::empty_like(x);
-
-    int blocks = (int)ceil((float)num_elements / BLOCK_SIZE);
-
-    if (at::isReducedFloatingType(x.scalar_type())) {
-        AT_DISPATCH_REDUCED_FLOATING_TYPES(
-            x.scalar_type(), "vector_addition_forward_kernel", ([&] {
-                vector_addition_forward_kernel<scalar_t><<<blocks, BLOCK_SIZE>>>(
-                    x.data<scalar_t>(), y.data<scalar_t>(), output.data<scalar_t>(), num_elements);
-            }));
-    } else {
-        AT_DISPATCH_FLOATING_TYPES(
-            x.scalar_type(), "vector_addition_forward_kernel", ([&] {
-                vector_addition_forward_kernel<scalar_t><<<blocks, BLOCK_SIZE>>>(
-                    x.data<scalar_t>(), y.data<scalar_t>(), output.data<scalar_t>(), num_elements);
-            }));
-    }
-
-    return output;
+void vector_addition_forward_kernel_dispatcher(
+    torch::Tensor x, torch::Tensor y, torch::Tensor output, const int NUM_BLOCKS, const int BLOCK_SIZE) {
+    AT_DISPATCH_FLOATING_TYPES_AND2(
+        at::ScalarType::Half, at::ScalarType::BFloat16, x.scalar_type(), "vector_addition_forward_kernel", ([&] {
+            vector_addition_forward_kernel<scalar_t><<<NUM_BLOCKS, BLOCK_SIZE>>>(
+                x.data<scalar_t>(), y.data<scalar_t>(), output.data<scalar_t>(), x.numel());
+        }));
 }
--- khd/vector_addition/cuda_implementation/ops.cpp
+++ khd/vector_addition/cuda_implementation/ops.cpp
@@ -1,7 +1,7 @@
 #include <torch/extension.h>
 
-// CUDA kernel declarations
-torch::Tensor vector_addition_forward_kernel_launcher(torch::Tensor x, torch::Tensor y, const int BLOCK_SIZE);
+void vector_addition_forward_kernel_dispatcher(
+    torch::Tensor x, torch::Tensor y, torch::Tensor output, const int NUM_BLOCKS, const int BLOCK_SIZE);
 
 torch::Tensor vector_addition_forward(torch::Tensor x, torch::Tensor y) {
     TORCH_CHECK(x.device().is_cuda(), "tensor x is not on GPU")
@@ -13,10 +13,19 @@ torch::Tensor vector_addition_forward(torch::Tensor x, torch::Tensor y) {
     TORCH_CHECK(x.dim() == 1, "tensor x should be 1 dimensional")
     TORCH_CHECK(y.dim() == 1, "tensor y should be 1 dimensional")
 
-    TORCH_CHECK(x.numel() == y.numel(), "both tensors should have same number of elements");
-    TORCH_CHECK(x.type() == y.type(), "both tensors should have same dtype");
+    int num_elements = x.numel();
 
-    return vector_addition_forward_kernel_launcher(x, y, 1024);
+    TORCH_CHECK(y.numel() == num_elements, "both tensors should have same number of elements");
+    TORCH_CHECK(x.scalar_type() == y.scalar_type(), "both tensors should have same dtype");
+
+    int BLOCK_SIZE = 1024;
+    int NUM_BLOCKS = (int)ceil((float)num_elements / BLOCK_SIZE);
+
+    torch::Tensor output = torch::empty_like(x);
+
+    vector_addition_forward_kernel_dispatcher(x, y, output, NUM_BLOCKS, BLOCK_SIZE);
+
+    return output;
 }
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
