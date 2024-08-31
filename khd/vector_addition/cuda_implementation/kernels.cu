c1669c74340b827d69e42b282038dbb526ebdf09
Mayank Mishra <mayank31398@gmail.com>
2024-08-31 02:25:49 -0400

    #define

--- khd/vector_addition/cuda_implementation/kernels.cu
+++ khd/vector_addition/cuda_implementation/kernels.cu
@@ -2,32 +2,36 @@
 #include <cuda_runtime.h>
 #include <torch/extension.h>
 
+#define BLOCK_SIZE 128
+#define NUM_ELEMENTS_PER_THREAD 4
+
 template <typename scalar_t>
 __global__ void vector_addition_forward_kernel(const scalar_t *x,
                                                const scalar_t *y,
                                                scalar_t *output,
-                                               const int total_elements,
-                                               const int num_elements_per_thread) {
+                                               const int num_elements) {
     int thread_id = blockIdx.x * blockDim.x + threadIdx.x;
 
 #pragma unroll
-    for (int i = 0; i < n; i++) {
-        int index = num_elements_per_thread * i + total_elements;
+    for (int i = 0; i < NUM_ELEMENTS_PER_THREAD; i++) {
+        int index = NUM_ELEMENTS_PER_THREAD * i + thread_id;
         if (index < num_elements) {
             output[index] = x[index] + y[index];
         }
     }
 }
 
-void vector_addition_forward_kernel_dispatcher(torch::Tensor x,
-                                               torch::Tensor y,
-                                               torch::Tensor output,
-                                               const int num_elements_per_thread,
-                                               const int NUM_BLOCKS,
-                                               const int BLOCK_SIZE) {
+torch::Tensor vector_addition_forward_kernel_dispatcher(torch::Tensor x, torch::Tensor y) {
+    int num_elements_per_block = BLOCK_SIZE * NUM_ELEMENTS_PER_THREAD;
+    int NUM_BLOCKS = (num_elements + num_elements_per_block - 1) / num_elements_per_block;
+
+    torch::Tensor output = torch::empty_like(x);
+
     AT_DISPATCH_FLOATING_TYPES_AND2(
         at::ScalarType::Half, at::ScalarType::BFloat16, x.scalar_type(), "vector_addition_forward_kernel", ([&] {
             vector_addition_forward_kernel<scalar_t><<<NUM_BLOCKS, BLOCK_SIZE>>>(
-                x.data<scalar_t>(), y.data<scalar_t>(), output.data<scalar_t>(), x.numel(), num_elements_per_thread);
+                x.data<scalar_t>(), y.data<scalar_t>(), output.data<scalar_t>(), x.numel());
         }));
+
+    return output;
 }
--- khd/vector_addition/cuda_implementation/ops.cpp
+++ khd/vector_addition/cuda_implementation/ops.cpp
@@ -1,11 +1,6 @@
 #include <torch/extension.h>
 
-void vector_addition_forward_kernel_dispatcher(torch::Tensor x,
-                                               torch::Tensor y,
-                                               torch::Tensor output,
-                                               const int num_elements_per_thread,
-                                               const int NUM_BLOCKS,
-                                               const int BLOCK_SIZE);
+torch::Tensor vector_addition_forward_kernel_dispatcher(torch::Tensor x, torch::Tensor y);
 
 torch::Tensor vector_addition_forward(torch::Tensor x, torch::Tensor y) {
     TORCH_CHECK(x.device().is_cuda(), "tensor x is not on GPU")
@@ -17,21 +12,10 @@ torch::Tensor vector_addition_forward(torch::Tensor x, torch::Tensor y) {
     TORCH_CHECK(x.dim() == 1, "tensor x should be 1 dimensional")
     TORCH_CHECK(y.dim() == 1, "tensor y should be 1 dimensional")
 
-    int num_elements = x.numel();
-
-    TORCH_CHECK(y.numel() == num_elements, "both tensors should have same number of elements");
+    TORCH_CHECK(x.numel() == y.numel(), "both tensors should have same number of elements");
     TORCH_CHECK(x.scalar_type() == y.scalar_type(), "both tensors should have same dtype");
 
-    const int num_elements_per_thread = 4;
-
-    int BLOCK_SIZE = 128;
-    int NUM_BLOCKS = (int)ceil((float)num_elements / (BLOCK_SIZE * num_elements_per_thread));
-
-    torch::Tensor output = torch::empty_like(x);
-
-    vector_addition_forward_kernel_dispatcher(x, y, output, num_elements_per_thread, NUM_BLOCKS, BLOCK_SIZE);
-
-    return output;
+    return vector_addition_forward_kernel_dispatcher(x, y);
 }
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
