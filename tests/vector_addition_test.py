beb7b7253240de22d99f50e7b51223d1b2d1a583
Mayank Mishra <mayank31398@gmail.com>
2024-09-09 17:59:53 -0400

    test 2d

--- tests/vector_addition_test.py
+++ tests/vector_addition_test.py
@@ -11,7 +11,7 @@ from .test_commons import TestCommons
 class VectorAdditionTest(TestCommons):
     @parameterized.expand(
         TestCommons.make_args_matrix(
-            TestCommons.get_1d_tensor_sizes(), [torch.device("cuda")], TestCommons.get_dtypes()
+            TestCommons.get_2d_tensor_sizes(), [torch.device("cuda")], TestCommons.get_dtypes()
         )
     )
     def test_vector_addition_cuda(self, size: int, device: torch.device, dtype: torch.dtype) -> None:
@@ -19,7 +19,7 @@ class VectorAdditionTest(TestCommons):
 
     @parameterized.expand(
         TestCommons.make_args_matrix(
-            TestCommons.get_1d_tensor_sizes(), [torch.device("cuda")], TestCommons.get_dtypes()
+            TestCommons.get_2d_tensor_sizes(), [torch.device("cuda")], TestCommons.get_dtypes()
         )
     )
     def test_vector_addition_triton(self, size: int, device: torch.device, dtype: torch.dtype) -> None:
