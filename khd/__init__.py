34bbfde27b0aee0487e15149a4fea8fe8e51459c
Mayank Mishra <mayank31398@gmail.com>
2024-09-09 14:36:43 -0400

    swiglu

--- khd/__init__.py
+++ khd/__init__.py
@@ -1,3 +1,4 @@
 from .kernel_registry import KernelRegistry
 from .scattermoe import MoE_Torch, MoE_Triton
+from .swiglu import swiglu_torch, swiglu_triton
 from .vector_addition import vector_addition_cuda, vector_addition_torch, vector_addition_triton
