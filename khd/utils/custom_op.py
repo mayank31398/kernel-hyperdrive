110cf462d692d3d26f4c699b38a31920bfb3325c
Mayank Mishra <mayank31398@gmail.com>
2024-09-13 10:58:48 -0400

    add comments
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- khd/utils/custom_op.py
+++ khd/utils/custom_op.py
@@ -4,12 +4,10 @@ import torch
 
 
 def library_custom_op(
-    func: Callable,
-    name: str,
-    mutates_args: Union[str, Iterable[str]],
+    name: str, fn: Callable | None = None, /, *, mutates_args: Union[str, Iterable[str]]
 ) -> Callable:
     @torch.library.custom_op(name, mutates_args=mutates_args)
     def f(*args, **kwargs):
-        return func(*args, **kwargs)
+        return fn(*args, **kwargs)
 
     return f
