64068606d4a419a92a3879021aca3a08b261558c
Mayank Mishra <mayank31398@gmail.com>
2024-09-16 01:19:02 -0400

    swiglu
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- khd/swiglu/cuda_implementation/__init__.py
+++ khd/swiglu/cuda_implementation/__init__.py
@@ -29,5 +29,17 @@ class _Swiglu_CUDA(torch.autograd.Function):
         return _Swiglu_CUDA.backward._kernel(gate, up, output_grad)
 
 
-def swiglu_cuda(gate: torch.Tensor, up: torch.Tensor) -> torch.Tensor:
-    return _Swiglu_CUDA.apply(gate, up)
+def swiglu_cuda(gate: torch.Tensor, up: torch.Tensor, memory_efficient: bool = False) -> torch.Tensor:
+    """swiglu
+
+    Args:
+        x (torch.Tensor): input tensor
+        y (torch.Tensor): input tensor
+        memory_efficient (bool, optional): whether to do an in-place op, uses `gate` and `up` to store gradients in the
+            backward pass if set to True. Defaults to False.
+
+    Returns:
+        torch.Tensor: output tensor
+    """
+
+    return _Swiglu_CUDA.apply(gate, up, memory_efficient)
