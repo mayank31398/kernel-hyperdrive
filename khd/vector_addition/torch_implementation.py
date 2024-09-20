dc963177f8dc0ca2bc1911cd997f6b49090568d6
Mayank Mishra <mayank31398@gmail.com>
2024-09-20 12:26:00 -0400

    drop mem-efficient

--- khd/vector_addition/torch_implementation.py
+++ khd/vector_addition/torch_implementation.py
@@ -1,22 +1,15 @@
 import torch
 
 
-def vector_addition_torch(x: torch.Tensor, y: torch.Tensor, memory_efficient: bool = False) -> torch.Tensor:
+def vector_addition_torch(x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
     """vector addition
 
     Args:
         x (torch.Tensor): input tensor
         y (torch.Tensor): input tensor
-        memory_efficient (bool, optional): whether to do an in-place op, will modify `x` if set to True. Defaults to False.
 
     Returns:
         torch.Tensor: output tensor
     """
 
-    if memory_efficient:
-        x += y
-        output = x
-    else:
-        output = x + y
-
-    return output
+    return x + y
--- khd/vector_addition/triton_implementation/ops.py
+++ khd/vector_addition/triton_implementation/ops.py
@@ -11,20 +11,14 @@ _KERNEL_NAME = "vector_addition_forward_triton"
 class _VectorAddition_Triton(torch.autograd.Function):
     @torch.profiler.record_function(f"{LIBRARY_NAME}:{_KERNEL_NAME}")
     @staticmethod
-    def forward(ctx, x: torch.Tensor, y: torch.Tensor, memory_efficient: bool) -> torch.Tensor:
+    def forward(ctx, x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
         assert x.is_cuda, "tensor x is not on GPU"
         assert y.is_cuda, "tensor y is not on GPU"
 
         assert x.size() == y.size(), "tensors x and y should have same shape"
         assert x.type() == y.type(), "tensors x and y should have same dtype"
 
-        if memory_efficient:
-            if x.is_leaf:
-                raise RuntimeError("leaf variables can't be used in an in-place operation")
-
-            output = x
-        else:
-            output = torch.empty_like(x)
+        output = torch.empty_like(x)
 
         num_elements = x.numel()
         grid = lambda meta: (triton.cdiv(num_elements, meta["BLOCK_SIZE"]),)
@@ -42,16 +36,15 @@ class _VectorAddition_Triton(torch.autograd.Function):
         return output_grad, output_grad, None
 
 
-def vector_addition_triton(x: torch.Tensor, y: torch.Tensor, memory_efficient: bool = False) -> torch.Tensor:
+def vector_addition_triton(x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
     """vector addition
 
     Args:
         x (torch.Tensor): input tensor
         y (torch.Tensor): input tensor
-        memory_efficient (bool, optional): whether to do an in-place op, will modify `x` if set to True. Defaults to False.
 
     Returns:
         torch.Tensor: output tensor
     """
 
-    return _VectorAddition_Triton.apply(x, y, memory_efficient)
+    return _VectorAddition_Triton.apply(x, y)
