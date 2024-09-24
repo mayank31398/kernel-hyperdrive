401751828b5407c571fbff93b9f014b0a1573463
Mayank Mishra <mayank31398@gmail.com>
2024-09-24 08:38:04 -0400

    move
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- khd/__init__.py
+++ khd/__init__.py
@@ -1,4 +1,4 @@
+from .add import vector_addition_cuda, vector_addition_torch, vector_addition_triton
 from .kernel_registry import KernelRegistry
 from .scattermoe import MoE_Torch, MoE_Triton
 from .swiglu import swiglu_torch, swiglu_triton
-from .vector_addition import vector_addition_cuda, vector_addition_torch, vector_addition_triton
