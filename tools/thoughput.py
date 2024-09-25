eaf82d5405b11d464aea85e6a5e3fd76ad7631b0
Mayank Mishra <32954280+mayank31398@users.noreply.github.com>
2024-09-25 16:53:53 -0400

    cleanup throughput file (#38)
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- tools/thoughput.py
+++ tools/thoughput.py
@@ -3,13 +3,15 @@ from time import perf_counter
 import torch
 from tabulate import tabulate
 
-from khd import vector_addition_cuda, vector_addition_torch, vector_addition_triton
+# from khd import add_scalar_cuda, add_scalar_torch, add_scalar_triton
+from khd import add_tensor_cuda, add_tensor_torch, add_tensor_triton
 
 
 n = 100
 
 headers = ["dtype", "torch", "cuda", "triton"]
-kernels = [vector_addition_torch, vector_addition_cuda, vector_addition_triton]
+# kernels = [add_scalar_torch, add_scalar_cuda, add_scalar_triton]
+kernels = [add_tensor_torch, add_tensor_cuda, add_tensor_triton]
 
 table = []
 
@@ -18,6 +20,7 @@ for dtype in [torch.float16, torch.bfloat16, torch.float32]:
     for kernel in kernels:
         # kernel = torch.compile(kernel)
         x = torch.randn(10485760, device=torch.cuda.current_device(), dtype=dtype)
+        # y = 0.42
         y = torch.randn(10485760, device=torch.cuda.current_device(), dtype=dtype)
 
         for i in range(n):
