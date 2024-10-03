df0b4ad58e1d18d23c52f1fce3c80088b80ca983
Mayank Mishra <mayank31398@gmail.com>
2024-10-03 14:42:44 -0400

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- khd/kernels/swiglu/cuda_implementation/ops.py
+++ khd/kernels/swiglu/cuda_implementation/ops.py
@@ -2,13 +2,14 @@ import torch
 
 from ....constants import LIBRARY_NAME
 from ....kernel_registry import KernelRegistry
+from ...utils import torch_custom_op
 
 
 _FORWARD_KERNEL_NAME = "swiglu_forward_cuda"
 _BACKWARD_KERNEL_NAME = "swiglu_backward_cuda"
 
 
-@torch.library.custom_op(f"{LIBRARY_NAME}::{_FORWARD_KERNEL_NAME}", mutates_args={})
+@torch_custom_op(f"{LIBRARY_NAME}::{_FORWARD_KERNEL_NAME}", mutates_args={})
 def _swiglu_forward_cuda_compilable(gate: torch.Tensor, up: torch.Tensor, BLOCK_SIZE: int) -> torch.Tensor:
     return KernelRegistry.get_kernel(_FORWARD_KERNEL_NAME)(gate, up, BLOCK_SIZE)
 
