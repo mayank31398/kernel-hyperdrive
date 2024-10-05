7f938d378d9e72ca9f9de71995030e30fdb00adc
Mayank Mishra <mayank31398@gmail.com>
2024-10-05 16:52:26 -0400

    &
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- khd/kernels/utils/dtypes.h
+++ khd/kernels/utils/dtypes.h
@@ -53,7 +53,7 @@ template <> struct DType<c10::Half> {
     using nv_dtype = fp16;
     using nv_dtype2 = fp16_2;
 
-    __device__ static nv_dtype2 reinterpret_32_bits_as_2x16(const fp32 value) {
+    __device__ static nv_dtype2 reinterpret_32_bits_as_2x16(const fp32 &value) {
         auto [lower_16, upper_16] = get_upper_and_lower_16_bits_from_fp32(value);
 
         nv_dtype lower_half = __ushort_as_half(lower_16);
@@ -62,7 +62,7 @@ template <> struct DType<c10::Half> {
         return __halves2half2(lower_half, upper_half);
     }
 
-    __device__ static fp32 reinterpret_2x16_as_32_bits(const nv_dtype2 value) {
+    __device__ static fp32 reinterpret_2x16_as_32_bits(const nv_dtype2 &value) {
         nv_dtype lower_half = __low2half(value);
         nv_dtype upper_half = __high2half(value);
 
@@ -90,7 +90,7 @@ template <> struct DType<c10::BFloat16> {
     using nv_dtype = bf16;
     using nv_dtype2 = bf16_2;
 
-    __device__ static nv_dtype2 reinterpret_32_bits_as_2x16(const fp32 value) {
+    __device__ static nv_dtype2 reinterpret_32_bits_as_2x16(const fp32 &value) {
         auto [lower_16, upper_16] = get_upper_and_lower_16_bits_from_fp32(value);
 
         nv_dtype lower_half = __ushort_as_bfloat16(lower_16);
@@ -99,7 +99,7 @@ template <> struct DType<c10::BFloat16> {
         return __halves2bfloat162(lower_half, upper_half);
     }
 
-    __device__ static fp32 reinterpret_2x16_as_32_bits(const nv_dtype2 value) {
+    __device__ static fp32 reinterpret_2x16_as_32_bits(const nv_dtype2 &value) {
         nv_dtype lower_half = __low2bfloat16(value);
         nv_dtype upper_half = __high2bfloat16(value);
 
