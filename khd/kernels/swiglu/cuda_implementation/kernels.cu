c53c6a79770aae6fc947e3a17cb7428eb9684b2d
Mayank Mishra <mayank31398@gmail.com>
2024-10-09 11:53:12 -0400

    custom dispatcher
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- khd/kernels/swiglu/cuda_implementation/kernels.cu
+++ khd/kernels/swiglu/cuda_implementation/kernels.cu
@@ -1,6 +1,6 @@
-#include "../../../utils/activations.h"
-#include "../../../utils/dtypes.h"
-#include "../../../utils/threads.h"
+#include "../../utils/activations.h"
+#include "../../utils/dtypes.h"
+#include "../../utils/threads.h"
 #include <cuda.h>
 #include <cuda_fp16.h>
 #include <cuda_runtime.h>
