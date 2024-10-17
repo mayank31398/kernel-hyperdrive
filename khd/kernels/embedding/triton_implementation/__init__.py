7f7631634df0461aa957667cf86dc10f35a0d90f
Mayank Mishra <mayank31398@gmail.com>
2024-10-17 16:26:15 -0400

    fix
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- khd/kernels/embedding/triton_implementation/__init__.py
+++ khd/kernels/embedding/triton_implementation/__init__.py
@@ -15,33 +15,27 @@ class _Embedding_Triton(torch.autograd.Function):
         assert input_ids.is_cuda, "tensor input_ids is not on GPU"
         assert wte.is_cuda, "tensor wte is not on GPU"
 
-        batch_size, sequence_length = input_ids.size()
+        num_tokens = input_ids.numel()
         hidden_size = wte.size(-1)
 
-        output = torch.empty(batch_size, sequence_length, hidden_size, device=input_ids.device)
+        output = torch.empty(*input_ids.size(), hidden_size, device=input_ids.device)
 
         grid = lambda meta: (
-            triton.cdiv(batch_size, meta["BLOCK_SIZE_B"]),
-            triton.cdiv(sequence_length, meta["BLOCK_SIZE_S"]),
+            triton.cdiv(num_tokens, meta["BLOCK_SIZE_B"]),
             triton.cdiv(hidden_size, meta["BLOCK_SIZE_H"]),
         )
 
         embedding_forward_triton_kernel[grid](
-            x_ptr=input_ids,
-            x_stride_b=input_ids.stride(0),
-            x_stride_s=input_ids.stride(1),
+            x_ptr=input_ids.view(-1),
             wte_ptr=wte,
             wte_stride_v=wte.stride(0),
             wte_stride_h=wte.stride(1),
-            logits_ptr=output,
-            logits_stride_b=output.stride(0),
-            logits_stride_s=output.stride(1),
-            logits_stride_h=output.stride(2),
-            B=batch_size,
-            S=sequence_length,
+            output_ptr=output.view(-1),
+            output_stride_b=output.stride(0),
+            output_stride_h=output.stride(1),
+            B=num_tokens,
             H=hidden_size,
             BLOCK_SIZE_B=64,
-            BLOCK_SIZE_S=64,
             BLOCK_SIZE_H=64,
         )
 
--- khd/kernels/embedding/triton_implementation/kernels.py
+++ khd/kernels/embedding/triton_implementation/kernels.py
@@ -5,50 +5,35 @@ import triton.language as tl
 @triton.jit
 def embedding_forward_triton_kernel(
     x_ptr,
-    x_stride_b,
-    x_stride_s,
     wte_ptr,
     wte_stride_v,
     wte_stride_h,
-    logits_ptr,
-    logits_stride_b,
-    logits_stride_s,
-    logits_stride_h,
+    output_ptr,
+    output_stride_b,
+    output_stride_h,
     B,
-    S,
     H,
     BLOCK_SIZE_B: tl.constexpr,
-    BLOCK_SIZE_S: tl.constexpr,
     BLOCK_SIZE_H: tl.constexpr,
 ):
     pid_b = tl.program_id(axis=0)
-    pid_s = tl.program_id(axis=1)
-    pid_h = tl.program_id(axis=2)
+    pid_h = tl.program_id(axis=1)
 
     block_start_b = pid_b * BLOCK_SIZE_B
-    block_start_s = pid_s * BLOCK_SIZE_S
     block_start_h = pid_h * BLOCK_SIZE_H
 
     indices_b = block_start_b + tl.arange(0, BLOCK_SIZE_B)
-    indices_s = block_start_s + tl.arange(0, BLOCK_SIZE_S)
     indices_h = block_start_h + tl.arange(0, BLOCK_SIZE_H)
 
     mask_b = indices_b < B
-    mask_s = indices_s < S
     mask_h = indices_h < H
-    mask_bs = mask_b[:, None] & mask_s[None, :]
+    mask_bs = mask_b[:, None] & mask_h[None, :]
 
-    x_ptrs = x_ptr + indices_b[:, None] * x_stride_b + indices_s[None, :] * x_stride_s
+    x_ptrs = x_ptr + indices_b
     x = tl.load(x_ptrs, mask=mask_bs)
 
-    wte_ptrs = wte_ptr + x[:, :, None] * wte_stride_v + indices_h[None, None, :] * wte_stride_h
-    word_embeddings = tl.load(wte_ptrs, mask=mask_h[None, None, :])
+    wte_ptrs = wte_ptr + x[:, None] * wte_stride_v + indices_h[None, :] * wte_stride_h
+    word_embeddings = tl.load(wte_ptrs, mask=mask_h[None, :])
 
-    logit_ptr = (
-        logits_ptr
-        + indices_b[:, None, None] * logits_stride_b
-        + indices_s[None, :, None] * logits_stride_s
-        + indices_h[None, None, :] * logits_stride_h
-    )
-
-    tl.store(logit_ptr, word_embeddings, mask=mask_bs[:, :, None] & mask_h[None, None, :])
+    output_ptrs = output_ptr + indices_b[:, None] * output_stride_b + indices_h[None, :] * output_stride_h
+    tl.store(output_ptrs, word_embeddings, mask=mask_bs[:, None] & mask_h[None, :])
