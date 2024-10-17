40b9ded4afe6a8b3de874cc745a00be04856647f
Mayank Mishra <mayank31398@gmail.com>
2024-10-16 23:30:30 -0400

    C++ formatting
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- tests/lightning_transformer_test.py
+++ tests/lightning_transformer_test.py
@@ -11,7 +11,7 @@ class LightningTransformerTest(TestCommons):
     @parameterized.expand(TestCommons.make_args_matrix([torch.device("cuda")]))
     def test_lightning_transformer(self, device: torch.device) -> None:
         vocab_size = 49152
-        input_ids = torch.randint(0, vocab_size, 100, 1000, device=device, dtype=torch.long)
+        input_ids = torch.randint(0, vocab_size, (100, 1000), device=device, dtype=torch.long)
         wte = nn.Embedding(vocab_size, 4096)
 
         assert wte(input_ids).equal(lightning_transformer_triton(input_ids, wte.weight))
