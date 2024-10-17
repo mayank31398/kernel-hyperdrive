822dba3dea7f863d18511db51802e06fda66c53b
Mayank Mishra <mayank31398@gmail.com>
2024-10-16 23:09:43 -0400

    C++ formatting
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- khd/lightning_transformer/triton_implementation/kernels.py
+++ khd/lightning_transformer/triton_implementation/kernels.py
@@ -19,7 +19,7 @@ def _get_word_embeddings(
     x_ptrs = x_ptr + indices_b[:, None] * x_stride_b + indices_s[None, :] * x_stride_s
     x = tl.load(x_ptrs, mask=mask_bs)
 
-    wte_ptrs = wte_ptr + x * wte_stride_v + indices_h[None, :] * wte_stride_h
+    wte_ptrs = wte_ptr + x[:, :, None] * wte_stride_v + indices_h[None, None, :] * wte_stride_h
     word_embeddings = tl.load(wte_ptrs, mask=mask_h)
 
     return word_embeddings
@@ -72,16 +72,3 @@ def lightning_transformer_triton_kernel(
     )
 
     tl.store(logits_ptr, word_embeddings, mask=mask_bs[:, :, None] & mask_h[None, None, :])
-
-
-# 2024-09-17 20:40:42,391 - [INFO    ] ▶ step = 10, train-loss = 10.5773, train-grad_norm = 106.5810, learning_rate = 1.0000e-04, train-FLOPs = 114.9260, train-billion_tokens_per_day = 3.6498, train-step_time (sec) = 6.2055
-# 2024-09-17 20:41:06,413 - [INFO    ] ▶ step = 20, train-loss = 7.4504, train-grad_norm = 14.6408, learning_rate = 2.0000e-04, train-FLOPs = 296.8974, train-billion_tokens_per_day = 9.4289, train-step_time (sec) = 2.4021
-# 2024-09-17 20:41:30,541 - [INFO    ] ▶ step = 30, train-loss = 6.2820, train-grad_norm = 4.9204, learning_rate = 3.0000e-04, train-FLOPs = 295.5880, train-billion_tokens_per_day = 9.3873, train-step_time (sec) = 2.4127
-
-
-# 2024-09-17 20:53:02,727 - [INFO    ] ▶ step = 60, train-loss = 5.6262, train-grad_norm = 4.1566, learning_rate = 6.0000e-04, train-FLOPs = 293.9193, train-billion_tokens_per_day = 9.3343, train-step_time (sec) = 2.4264
-# 2024-09-17 20:53:26,994 - [INFO    ] ▶ step = 70, train-loss = 5.3426, train-grad_norm = 3.7945, learning_rate = 7.0000e-04, train-FLOPs = 293.8937, train-billion_tokens_per_day = 9.3335, train-step_time (sec) = 2.4267
-# 2024-09-17 20:53:51,450 - [INFO    ] ▶ step = 80, train-loss = 5.0332, train-grad_norm = 4.0429, learning_rate = 8.0000e-04, train-FLOPs = 291.6207, train-billion_tokens_per_day = 9.2613, train-step_time (sec) = 2.4456
-# 2024-09-17 20:54:15,574 - [INFO    ] ▶ step = 90, train-loss = 4.7266, train-grad_norm = 3.5853, learning_rate = 9.0000e-04, train-FLOPs = 295.6335, train-billion_tokens_per_day = 9.3888, train-step_time (sec) = 2.4124
-# 2024-09-17 20:54:39,677 - [INFO    ] ▶ step = 100, train-loss = 4.4518, train-grad_norm = 3.4109, learning_rate = 1.0000e-03, train-FLOPs = 295.8840, train-billion_tokens_per_day = 9.3967, train-step_time (sec) = 2.4103
-# 2024-09-17 20:55:03,768 - [INFO    ] ▶ step = 110, train-loss = 4.3172, train-grad_norm = 3.5594, learning_rate = 1.1000e-03, train-FLOPs = 296.0478, train-billion_tokens_per_day = 9.4019, train-step_time (sec) = 2.4090
