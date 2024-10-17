01e1ce41c54f329fdc205bfc24388e6d80013b94
Mayank Mishra <32954280+mayank31398@users.noreply.github.com>
2024-10-17 17:52:43 -0400

    word embeddings (#49)

--- khd/kernels/__init__.py
+++ khd/kernels/__init__.py
@@ -6,5 +6,6 @@ from .add import (
     add_tensor_torch,
     add_tensor_triton,
 )
+from .embedding import embedding_torch, embedding_triton
 from .scattermoe import MoE_Torch, MoE_Triton
 from .swiglu import swiglu_cuda, swiglu_torch, swiglu_triton
