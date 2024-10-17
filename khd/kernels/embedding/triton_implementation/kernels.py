61630a68c53b21ae6e8cfabc2e33a337dc733088
Mayank Mishra <mayank31398@gmail.com>
2024-10-17 21:18:58 +0000

    &

--- khd/kernels/embedding/triton_implementation/kernels.py
+++ khd/kernels/embedding/triton_implementation/kernels.py
@@ -27,13 +27,12 @@ def embedding_forward_triton_kernel(
 
     mask_b = indices_b < B
     mask_h = indices_h < H
-    mask_bs = mask_b[:, None] & mask_h[None, :]
 
     x_ptrs = x_ptr + indices_b
-    x = tl.load(x_ptrs, mask=mask_bs)
+    x = tl.load(x_ptrs, mask=mask_b)
 
     wte_ptrs = wte_ptr + x[:, None] * wte_stride_v + indices_h[None, :] * wte_stride_h
     word_embeddings = tl.load(wte_ptrs, mask=mask_h[None, :])
 
     output_ptrs = output_ptr + indices_b[:, None] * output_stride_b + indices_h[None, :] * output_stride_h
-    tl.store(output_ptrs, word_embeddings, mask=mask_bs[:, None] & mask_h[None, :])
+    tl.store(output_ptrs, word_embeddings, mask=mask_b[:, None] & mask_h[None, :])
