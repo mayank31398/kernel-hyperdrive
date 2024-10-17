d48c9388389c4a02aa9948229b22a3d3ec5a5a59
Mayank Mishra <mayank31398@gmail.com>
2024-10-16 23:29:59 -0400

    C++ formatting
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- tests/lightning_transformer_test.py
+++ tests/lightning_transformer_test.py
@@ -1,5 +1,3 @@
-from typing import Callable
-
 import torch
 import torch.nn as nn
 from parameterized import parameterized
@@ -10,15 +8,8 @@ from .test_commons import TestCommons
 
 
 class LightningTransformerTest(TestCommons):
-    @parameterized.expand(
-        TestCommons.make_args_matrix(
-            TestCommons.get_2d_tensor_sizes(),
-            [torch.device("cuda")],
-        )
-    )
-    def test_lightning_transformer(
-        self, size: tuple[int], device: torch.device, dtype: torch.dtype, function: Callable
-    ) -> None:
+    @parameterized.expand(TestCommons.make_args_matrix([torch.device("cuda")]))
+    def test_lightning_transformer(self, device: torch.device) -> None:
         vocab_size = 49152
         input_ids = torch.randint(0, vocab_size, 100, 1000, device=device, dtype=torch.long)
         wte = nn.Embedding(vocab_size, 4096)
