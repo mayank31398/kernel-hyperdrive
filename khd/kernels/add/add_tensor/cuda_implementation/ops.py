0b60fae9225c1418d8c46b0e84058c9929d36e30
Mayank Mishra <mayank31398@gmail.com>
2024-10-18 12:27:41 -0400

    newlines
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- khd/kernels/add/add_tensor/cuda_implementation/ops.py
+++ khd/kernels/add/add_tensor/cuda_implementation/ops.py
@@ -2,18 +2,12 @@ import torch
 
 from .....constants import LIBRARY_NAME
 from .....kernel_registry import KernelRegistry
-from .....utils import AutoTune, get_vectorized_autotune_configs
 from ....utils import torch_custom_op
 
 
 _KERNEL_NAME = "add_tensor_forward_cuda"
 
 
-@AutoTune(
-    configs=get_vectorized_autotune_configs(
-        extra_config_condition=lambda **kwargs: kwargs["x"].dtype in [torch.float16, torch.bfloat16]
-    )
-)
 def _add_tensor_forward_cuda(
     x: torch.Tensor, y: torch.Tensor, vectorized_loop_size: int, BLOCK_SIZE: int
 ) -> torch.Tensor:
@@ -21,8 +15,10 @@ def _add_tensor_forward_cuda(
 
 
 @torch_custom_op(f"{LIBRARY_NAME}::{_KERNEL_NAME}", mutates_args={})
-def _add_tensor_forward_cuda_compilable(x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
-    return _add_tensor_forward_cuda(x, y)
+def _add_tensor_forward_cuda_compilable(
+    x: torch.Tensor, y: torch.Tensor, vectorized_loop_size: int, BLOCK_SIZE: int
+) -> torch.Tensor:
+    return _add_tensor_forward_cuda(x=x, y=y, vectorized_loop_size=vectorized_loop_size, BLOCK_SIZE=BLOCK_SIZE)
 
 
 @_add_tensor_forward_cuda_compilable.register_fake
