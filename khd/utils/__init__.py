c991913cd59d97df748762e0562ed8dd3da519fa
Mayank Mishra <mayank31398@gmail.com>
2024-10-21 09:42:48 -0400

    CutoTune
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- khd/utils/__init__.py
+++ khd/utils/__init__.py
@@ -2,7 +2,6 @@ from .autotune import (
     Config,
     CutoTune,
     ensure_same_strides,
-    force_contiguous,
     get_default_cuda_autotune_configs,
     get_default_triton_autotune_configs,
 )
