8856f523c8ebbf1b392c1fce3658ade2ed5524a7
Mayank Mishra <mayank31398@gmail.com>
2024-11-02 15:35:08 -0400

    loss
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- khd/kernels/add/add_scalar/cuda_implementation/__init__.py
+++ khd/kernels/add/add_scalar/cuda_implementation/__init__.py
@@ -1,1 +1,38 @@
-from .ops import add_scalar_cuda
+import torch
+
+from .ops import _add_scalar_forward_cuda, _add_scalar_forward_cuda_compilable
+
+
+class _AddScalar_CUDA(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, x: torch.Tensor, y: float, BLOCK_SIZE: int) -> torch.Tensor:
+        if y == 0:
+            return x
+
+        output = torch.empty_like(x)
+
+        if torch.compiler.is_compiling():
+            _add_scalar_forward_cuda_compilable(x=x, y=y, output=output, BLOCK_SIZE=BLOCK_SIZE)
+        else:
+            _add_scalar_forward_cuda(x=x, y=y, output=output, BLOCK_SIZE=BLOCK_SIZE)
+
+        return output
+
+    @staticmethod
+    def backward(ctx, output_grad: torch.Tensor) -> tuple[torch.Tensor | None]:
+        return output_grad, None, None
+
+
+def add_scalar_cuda(x: torch.Tensor, y: float, BLOCK_SIZE: int) -> torch.Tensor:
+    """tensor addition
+
+    Args:
+        x (torch.Tensor): input tensor
+        y (float): input scalar
+        BLOCK_SIZE (int): block size
+
+    Returns:
+        torch.Tensor: output tensor
+    """
+
+    return _AddScalar_CUDA.apply(x, y, BLOCK_SIZE)
--- khd/kernels/add/add_scalar/cuda_implementation/ops.py
+++ khd/kernels/add/add_scalar/cuda_implementation/ops.py
@@ -15,38 +15,3 @@ def _add_scalar_forward_cuda(x: torch.Tensor, y: float, output: torch.Tensor, BL
 @torch_custom_op(f"{LIBRARY_NAME}::{_KERNEL_NAME}", mutates_args={"output"})
 def _add_scalar_forward_cuda_compilable(x: torch.Tensor, y: float, output: torch.Tensor, BLOCK_SIZE: int) -> None:
     _add_scalar_forward_cuda(x, y, output, BLOCK_SIZE)
-
-
-class _AddScalar_CUDA(torch.autograd.Function):
-    @staticmethod
-    def forward(ctx, x: torch.Tensor, y: float, BLOCK_SIZE: int) -> torch.Tensor:
-        if y == 0:
-            return x
-
-        output = torch.empty_like(x)
-
-        if torch.compiler.is_compiling():
-            _add_scalar_forward_cuda_compilable(x=x, y=y, output=output, BLOCK_SIZE=BLOCK_SIZE)
-        else:
-            _add_scalar_forward_cuda(x=x, y=y, output=output, BLOCK_SIZE=BLOCK_SIZE)
-
-        return output
-
-    @staticmethod
-    def backward(ctx, output_grad: torch.Tensor) -> tuple[torch.Tensor | None]:
-        return output_grad, None, None
-
-
-def add_scalar_cuda(x: torch.Tensor, y: float, BLOCK_SIZE: int) -> torch.Tensor:
-    """tensor addition
-
-    Args:
-        x (torch.Tensor): input tensor
-        y (float): input scalar
-        BLOCK_SIZE (int): block size
-
-    Returns:
-        torch.Tensor: output tensor
-    """
-
-    return _AddScalar_CUDA.apply(x, y, BLOCK_SIZE)
