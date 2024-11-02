5d89eb3f8fc739f0bc24a59d14ff65db50875c39
Mayank Mishra <mayank31398@gmail.com>
2024-11-02 15:43:12 -0400

    loss
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- khd/kernels/swiglu/cuda_implementation/__init__.py
+++ khd/kernels/swiglu/cuda_implementation/__init__.py
@@ -1,1 +1,55 @@
-from .ops import swiglu_cuda
+import torch
+
+from ....constants import LIBRARY_NAME
+from ....kernel_registry import KernelRegistry
+from .ops import _sw, _swiglu_backward_cuda_compilable, _swiglu_forward_cuda_compilable
+
+
+_FORWARD_KERNEL_NAME = "swiglu_forward_cuda"
+_BACKWARD_KERNEL_NAME = "swiglu_backward_cuda"
+
+
+class _Swiglu_CUDA(torch.autograd.Function):
+    @torch.profiler.record_function(f"{LIBRARY_NAME}:{_FORWARD_KERNEL_NAME}")
+    @staticmethod
+    def forward(ctx, gate: torch.Tensor, up: torch.Tensor) -> torch.Tensor:
+        ctx.save_for_backward(gate, up)
+
+        BLOCK_SIZE = 1024
+
+        if torch.compiler.is_compiling():
+            output = _swiglu_forward_cuda_compilable(gate, up, BLOCK_SIZE)
+        else:
+            kernel = KernelRegistry.get_kernel(_FORWARD_KERNEL_NAME)
+            output = kernel(gate, up, BLOCK_SIZE)
+
+        return output
+
+    @torch.profiler.record_function(f"{LIBRARY_NAME}:{_BACKWARD_KERNEL_NAME}")
+    @staticmethod
+    def backward(ctx, output_grad: torch.Tensor) -> tuple[torch.Tensor | None]:
+        gate, up = ctx.saved_tensors
+
+        BLOCK_SIZE = 1024
+
+        if torch.compiler.is_compiling():
+            gate_grad, up_grad = _swiglu_backward_cuda_compilable(gate, up, BLOCK_SIZE)
+        else:
+            kernel = KernelRegistry.get_kernel(_BACKWARD_KERNEL_NAME)
+            gate_grad, up_grad = kernel(gate, up, output_grad, BLOCK_SIZE)
+
+        return gate_grad, up_grad
+
+
+def swiglu_cuda(gate: torch.Tensor, up: torch.Tensor) -> torch.Tensor:
+    """swiglu
+
+    Args:
+        gate (torch.Tensor): gate tensor
+        up (torch.Tensor): up tensor
+
+    Returns:
+        torch.Tensor: output tensor
+    """
+
+    return _Swiglu_CUDA.apply(gate, up)
