bbc94a7bbbac1e2869d567ae120ce874d9a6c43c
Mayank Mishra <mayank31398@gmail.com>
2024-11-03 01:23:42 -0500

    loss
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- khd/kernels/swiglu/cuda_implementation/__init__.py
+++ khd/kernels/swiglu/cuda_implementation/__init__.py
@@ -9,7 +9,9 @@ KernelRegistry.get_kernel("swiglu_backward_cuda")
 
 class _Swiglu_CUDA(torch.autograd.Function):
     @staticmethod
-    def forward(ctx, gate: torch.Tensor, up: torch.Tensor, BLOCK_SIZE_forward: int) -> torch.Tensor:
+    def forward(
+        ctx, gate: torch.Tensor, up: torch.Tensor, BLOCK_SIZE_forward: int, BLOCK_SIZE_backward: int
+    ) -> torch.Tensor:
         assert gate.is_cuda, "tensor gate is not on GPU"
         assert up.is_cuda, "tensor up is not on GPU"
 
@@ -17,6 +19,7 @@ class _Swiglu_CUDA(torch.autograd.Function):
         assert gate.type() == up.type(), "tensors gate and up should have same dtype"
 
         ctx.save_for_backward(gate, up)
+        ctx.BLOCK_SIZE_backward = BLOCK_SIZE_backward
 
         output = torch.empty_like(gate)
 
@@ -28,23 +31,26 @@ class _Swiglu_CUDA(torch.autograd.Function):
     def backward(ctx, output_grad: torch.Tensor) -> tuple[torch.Tensor | None]:
         gate, up = ctx.saved_tensors
 
-        BLOCK_SIZE = 1024
-
         gate_grad = torch.empty_like(gate)
         up_grad = torch.empty_like(up)
 
-        gate_grad, up_grad = torch.ops.khd.swiglu_backward_cuda(gate, up, output_grad, gate_grad, up_grad, BLOCK_SIZE)
+        gate_grad, up_grad = torch.ops.khd.swiglu_backward_cuda(
+            gate, up, output_grad, gate_grad, up_grad, ctx.BLOCK_SIZE_backward
+        )
 
         return gate_grad, up_grad, None
 
 
-def swiglu_cuda(gate: torch.Tensor, up: torch.Tensor, BLOCK_SIZE_forward: int) -> torch.Tensor:
+def swiglu_cuda(
+    gate: torch.Tensor, up: torch.Tensor, BLOCK_SIZE_forward: int, BLOCK_SIZE_backward: int
+) -> torch.Tensor:
     """swiglu
 
     Args:
         gate (torch.Tensor): gate tensor
         up (torch.Tensor): up tensor
         BLOCK_SIZE_forward (int): forward block size
+        BLOCK_SIZE_backward (int): backward block size
 
     Returns:
         torch.Tensor: output tensor
