a4676fc601ae27fe5a48e638d6cd9ecfe99ea9e3
Mayank Mishra <mayank31398@gmail.com>
2024-11-02 23:27:03 -0400

    loss
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- khd/kernels/swiglu/cuda_implementation/__init__.py
+++ khd/kernels/swiglu/cuda_implementation/__init__.py
@@ -2,10 +2,35 @@ import torch
 
 from ....constants import LIBRARY_NAME
 from ....kernel_registry import KernelRegistry
+from ...utils import torch_custom_op
 
 
 _FORWARD_KERNEL_NAME = "swiglu_forward_cuda"
 _BACKWARD_KERNEL_NAME = "swiglu_backward_cuda"
+
+
+@torch_custom_op(f"{LIBRARY_NAME}::{_FORWARD_KERNEL_NAME}", mutates_args={})
+def swiglu_forward_cuda(gate: torch.Tensor, up: torch.Tensor, BLOCK_SIZE: int) -> torch.Tensor:
+    return KernelRegistry.get_kernel(_FORWARD_KERNEL_NAME)(gate, up, BLOCK_SIZE)
+
+
+@swiglu_forward_cuda.register_fake
+def _(gate: torch.Tensor, up: torch.Tensor, BLOCK_SIZE: int) -> torch.Tensor:
+    return torch.empty_like(gate)
+
+
+@torch_custom_op(f"{LIBRARY_NAME}::{_BACKWARD_KERNEL_NAME}", mutates_args={})
+def swiglu_backward_cuda(
+    gate: torch.Tensor, up: torch.Tensor, output_grad: torch.Tensor, BLOCK_SIZE: int
+) -> tuple[torch.Tensor, torch.Tensor]:
+    return KernelRegistry.get_kernel(_BACKWARD_KERNEL_NAME)(gate, up, output_grad, BLOCK_SIZE)
+
+
+@swiglu_backward_cuda.register_fake
+def _(
+    gate: torch.Tensor, up: torch.Tensor, output_grad: torch.Tensor, BLOCK_SIZE: int
+) -> tuple[torch.Tensor, torch.Tensor]:
+    return torch.empty_like(gate), torch.empty_like(gate)
 
 
 class _Swiglu_CUDA(torch.autograd.Function):
@@ -16,7 +41,7 @@ class _Swiglu_CUDA(torch.autograd.Function):
 
         BLOCK_SIZE = 1024
 
-        output = KernelRegistry.get_kernel(_FORWARD_KERNEL_NAME)(gate, up, BLOCK_SIZE)
+        output = torch.ops.khd.swiglu_forward_cuda(gate, up, BLOCK_SIZE)
 
         return output
 
@@ -27,7 +52,7 @@ class _Swiglu_CUDA(torch.autograd.Function):
 
         BLOCK_SIZE = 1024
 
-        gate_grad, up_grad = KernelRegistry.get_kernel(_BACKWARD_KERNEL_NAME)(gate, up, output_grad, BLOCK_SIZE)
+        gate_grad, up_grad = torch.ops.khd.swiglu_backward_cuda(gate, up, output_grad, BLOCK_SIZE)
 
         return gate_grad, up_grad
 
