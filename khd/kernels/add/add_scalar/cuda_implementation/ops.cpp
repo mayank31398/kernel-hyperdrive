9efbfa2a00dcacb6ff0ba5db712481aace4a2fd3
Mayank Mishra <mayank31398@gmail.com>
2024-11-02 23:33:59 -0400

    loss
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- khd/kernels/add/add_scalar/cuda_implementation/ops.cpp
+++ khd/kernels/add/add_scalar/cuda_implementation/ops.cpp
@@ -4,7 +4,7 @@ void add_scalar_forward_cuda(const torch::Tensor x, const double &y, torch::Tens
 
 TORCH_LIBRARY(khd, m) { m.def("add_scalar_forward_cuda(Tensor x, float y, Tensor output, int BLOCK_SIZE) -> ()"); }
 
-TORCH_LIBRARY_IMPL(khd, CUDA, m) { m.def("add_scalar_forward_cuda", add_scalar_forward_cuda); }
+TORCH_LIBRARY_IMPL(khd, CUDA, m) { m.impl("add_scalar_forward_cuda", add_scalar_forward_cuda); }
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
     m.def("add_scalar_forward_cuda", &add_scalar_forward_cuda, "Scalar addition forward (CUDA)");
