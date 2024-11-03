7fbe2b5fb2a0b522b006353e1c135eca8dc6a052
Mayank Mishra <mayank31398@gmail.com>
2024-11-03 14:09:52 -0500

    loss
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- khd/kernels/add/add_scalar/cuda_implementation/__init__.py
+++ khd/kernels/add/add_scalar/cuda_implementation/__init__.py
@@ -1,9 +1,19 @@
 import torch
 
+from .....constants import LIBRARY_NAME
 from .....kernel_registry import KernelRegistry
 
 
-KernelRegistry.get_kernel("add_scalar_forward_cuda")
+_KERNEL_NAME = "add_scalar_forward_cuda"
+
+
+def _add_scalar_forward_cuda(x: torch.Tensor, y: float, output: torch.Tensor, BLOCK_SIZE: int) -> None:
+    KernelRegistry.get_kernel(_KERNEL_NAME)(x, y, output, BLOCK_SIZE)
+
+
+@torch.library.custom_op(f"{LIBRARY_NAME}::{_KERNEL_NAME}", mutates_args={"output"})
+def _add_scalar_forward_cuda_compileable(x: torch.Tensor, y: float, output: torch.Tensor, BLOCK_SIZE: int) -> None:
+    _add_scalar_forward_cuda(x=x, y=y, output=output, BLOCK_SIZE=BLOCK_SIZE)
 
 
 class _AddScalar_CUDA(torch.autograd.Function):
@@ -14,7 +24,10 @@ class _AddScalar_CUDA(torch.autograd.Function):
 
         output = torch.empty_like(x)
 
-        torch.ops.khd.add_scalar_forward_cuda(x=x, y=y, output=output, BLOCK_SIZE=BLOCK_SIZE)
+        if torch.compiler.is_compiling():
+            _add_scalar_forward_cuda_compileable(x=x, y=y, output=output, BLOCK_SIZE=BLOCK_SIZE)
+        else:
+            _add_scalar_forward_cuda(x=x, y=y, output=output, BLOCK_SIZE=BLOCK_SIZE)
 
         return output
 
