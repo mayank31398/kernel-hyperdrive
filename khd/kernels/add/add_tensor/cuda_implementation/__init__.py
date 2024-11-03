9902a239c717fc2a328a3d0a9aebf1b767e335fc
Mayank Mishra <mayank31398@gmail.com>
2024-11-03 13:31:22 -0500

    loss
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- khd/kernels/add/add_tensor/cuda_implementation/__init__.py
+++ khd/kernels/add/add_tensor/cuda_implementation/__init__.py
@@ -1,9 +1,25 @@
 import torch
 
+from .....constants import LIBRARY_NAME
 from .....kernel_registry import KernelRegistry
 
 
-KernelRegistry.get_kernel("add_tensor_forward_cuda")
+_KERNEL_NAME = "add_tensor_forward_cuda"
+
+
+def _add_tensor_forward_cuda(
+    x: torch.Tensor, y: torch.Tensor, output: torch.Tensor, vectorized_loop_size: int, BLOCK_SIZE: int
+) -> None:
+    KernelRegistry.get_kernel(_KERNEL_NAME)(
+        x=x, y=y, output=output, vectorized_loop_size=vectorized_loop_size, BLOCK_SIZE=BLOCK_SIZE
+    )
+
+
+@torch.library.custom_op(f"{LIBRARY_NAME}::{_KERNEL_NAME}", mutates_args={"output"})
+def _add_tensor_forward_cuda_compileable(
+    x: torch.Tensor, y: torch.Tensor, output: torch.Tensor, vectorized_loop_size: int, BLOCK_SIZE: int
+) -> None:
+    _add_tensor_forward_cuda(x=x, y=y, output=output, vectorized_loop_size=vectorized_loop_size, BLOCK_SIZE=BLOCK_SIZE)
 
 
 class _AddTensor_CUDA(torch.autograd.Function):
@@ -17,9 +33,14 @@ class _AddTensor_CUDA(torch.autograd.Function):
 
         output = torch.empty_like(x)
 
-        torch.ops.khd.add_tensor_forward_cuda(
-            x=x, y=y, output=output, vectorized_loop_size=vectorized_loop_size, BLOCK_SIZE=BLOCK_SIZE
-        )
+        if torch.compiler.is_compiling():
+            _add_tensor_forward_cuda_compileable(
+                x=x, y=y, output=output, vectorized_loop_size=vectorized_loop_size, BLOCK_SIZE=BLOCK_SIZE
+            )
+        else:
+            _add_tensor_forward_cuda(
+                x=x, y=y, output=output, vectorized_loop_size=vectorized_loop_size, BLOCK_SIZE=BLOCK_SIZE
+            )
 
         return output
 
