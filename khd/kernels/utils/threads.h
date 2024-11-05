5e8016a731a4c5798c6d54b3c929f77a3811f020
Mayank Mishra <mayank31398@gmail.com>
2024-11-05 03:17:39 -0500

    loss
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- khd/kernels/utils/threads.h
+++ khd/kernels/utils/threads.h
@@ -3,13 +3,11 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
 
-__device__ unsigned int get_threads_per_block() { return blockDim.x * blockDim.y * blockDim.z; }
+__device__ uint get_threads_per_block() { return blockDim.x * blockDim.y * blockDim.z; }
 
-__device__ unsigned int get_num_blocks() { return gridDim.x * gridDim.y * gridDim.z; }
+__device__ uint get_num_blocks() { return gridDim.x * gridDim.y * gridDim.z; }
 
-__device__ unsigned int get_block_id() {
-    return gridDim.x * gridDim.y * blockIdx.z + gridDim.x * blockIdx.y + blockIdx.x;
-}
+__device__ uint get_block_id() { return gridDim.x * gridDim.y * blockIdx.z + gridDim.x * blockIdx.y + blockIdx.x; }
 
 __device__ int64_t get_local_thread_id() {
     return blockDim.x * blockDim.y * threadIdx.z + blockDim.x * threadIdx.y + threadIdx.x;
