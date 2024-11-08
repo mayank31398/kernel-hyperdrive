6b4c1f2390d618db882c3b871dae9bab21941701
Mayank Mishra <mayank31398@gmail.com>
2024-11-07 23:25:28 -0500

    wrapper
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- khd/kernels/utils/threads.h
+++ khd/kernels/utils/threads.h
@@ -9,7 +9,7 @@ __device__ uint get_num_blocks() { return gridDim.x * gridDim.y * gridDim.z; }
 
 __device__ uint get_block_id() { return gridDim.x * gridDim.y * blockIdx.z + gridDim.x * blockIdx.y + blockIdx.x; }
 
-__device__ int64_t get_local_thread_id() {
+__device__ uint get_local_thread_id() {
     return blockDim.x * blockDim.y * threadIdx.z + blockDim.x * threadIdx.y + threadIdx.x;
 }
 
