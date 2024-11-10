190cd9fc17fcee6536d93ea09dfa250a99c10508
Mayank Mishra <32954280+mayank31398@users.noreply.github.com>
2024-11-10 16:32:37 -0500

    Vector cleanup for tensor add (#56)

--- khd/kernels/add/add_tensor/cuda_implementation/__init__.py
+++ khd/kernels/add/add_tensor/cuda_implementation/__init__.py
@@ -7,16 +7,12 @@ from .....kernel_registry import KernelRegistry
 _KERNEL_NAME = "add_tensor_forward_cuda"
 
 
-def add_tensor_forward_cuda_kernel(
-    x: torch.Tensor, y: torch.Tensor, output: torch.Tensor, vector_instruction_width: int, BLOCK_SIZE: int
-) -> None:
-    KernelRegistry.get_kernel(_KERNEL_NAME)(x, y, output, vector_instruction_width, BLOCK_SIZE)
+def add_tensor_forward_cuda_kernel(x: torch.Tensor, y: torch.Tensor, output: torch.Tensor, BLOCK_SIZE: int) -> None:
+    KernelRegistry.get_kernel(_KERNEL_NAME)(x, y, output, BLOCK_SIZE)
 
 
 @torch.library.custom_op(f"{LIBRARY_NAME}::{_KERNEL_NAME}", mutates_args={"output"})
 def add_tensor_forward_cuda_kernel_compileable(
-    x: torch.Tensor, y: torch.Tensor, output: torch.Tensor, vector_instruction_width: int, BLOCK_SIZE: int
+    x: torch.Tensor, y: torch.Tensor, output: torch.Tensor, BLOCK_SIZE: int
 ) -> None:
-    add_tensor_forward_cuda_kernel(
-        x=x, y=y, output=output, vector_instruction_width=vector_instruction_width, BLOCK_SIZE=BLOCK_SIZE
-    )
+    add_tensor_forward_cuda_kernel(x=x, y=y, output=output, BLOCK_SIZE=BLOCK_SIZE)
