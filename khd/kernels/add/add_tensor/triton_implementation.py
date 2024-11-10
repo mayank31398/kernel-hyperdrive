a38b2bd97566d8af1f1e241c76925c96f062dcdd
Mayank Mishra <mayank31398@gmail.com>
2024-11-09 23:06:03 -0500

    reformat
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- khd/kernels/add/add_tensor/triton_implementation.py
+++ khd/kernels/add/add_tensor/triton_implementation.py
@@ -5,14 +5,24 @@ import triton.language as tl
 @triton.jit
 def add_tensor_forward_triton_kernel(x_ptr, y_ptr, output_ptr, num_elements, BLOCK_SIZE: tl.constexpr):
     pid = tl.program_id(axis=0)
+    num_blocks = tl.num_programs(axis=0)
+    is_last_block = pid == num_blocks - 1
 
     block_start = pid * BLOCK_SIZE
     indices = block_start + tl.arange(0, BLOCK_SIZE)
-    mask = indices < num_elements
 
-    x = tl.load(x_ptr + indices, mask=mask)
-    y = tl.load(y_ptr + indices, mask=mask)
+    if is_last_block:
+        mask = indices < num_elements
+
+        x = tl.load(x_ptr + indices, mask=mask)
+        y = tl.load(y_ptr + indices, mask=mask)
+    else:
+        x = tl.load(x_ptr + indices)
+        y = tl.load(y_ptr + indices)
 
     output = x + y
 
-    tl.store(output_ptr + indices, output, mask=mask)
+    if is_last_block:
+        tl.store(output_ptr + indices, output, mask=mask)
+    else:
+        tl.store(output_ptr + indices, output)
