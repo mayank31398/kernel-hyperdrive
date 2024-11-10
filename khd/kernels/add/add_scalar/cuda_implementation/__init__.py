83928c4930a0e843ef483277dc616cc47fe233f8
Mayank Mishra <mayank31398@gmail.com>
2024-11-10 16:49:34 -0500

    reformat
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- khd/kernels/add/add_scalar/cuda_implementation/__init__.py
+++ khd/kernels/add/add_scalar/cuda_implementation/__init__.py
@@ -7,16 +7,12 @@ from .....kernel_registry import KernelRegistry
 _KERNEL_NAME = "add_scalar_forward_cuda"
 
 
-def add_scalar_forward_cuda_kernel(
-    x: torch.Tensor, y: float, output: torch.Tensor, vector_instruction_width: int, BLOCK_SIZE: int
-) -> None:
-    KernelRegistry.get_kernel(_KERNEL_NAME)(x, y, output, vector_instruction_width, BLOCK_SIZE)
+def add_scalar_forward_cuda_kernel(x: torch.Tensor, y: float, output: torch.Tensor, BLOCK_SIZE: int) -> None:
+    KernelRegistry.get_kernel(_KERNEL_NAME)(x, y, output, BLOCK_SIZE)
 
 
 @torch.library.custom_op(f"{LIBRARY_NAME}::{_KERNEL_NAME}", mutates_args={"output"})
 def add_scalar_forward_cuda_kernel_compileable(
-    x: torch.Tensor, y: float, output: torch.Tensor, vector_instruction_width: int, BLOCK_SIZE: int
+    x: torch.Tensor, y: float, output: torch.Tensor, BLOCK_SIZE: int
 ) -> None:
-    add_scalar_forward_cuda_kernel(
-        x=x, y=y, output=output, vector_instruction_width=vector_instruction_width, BLOCK_SIZE=BLOCK_SIZE
-    )
+    add_scalar_forward_cuda_kernel(x=x, y=y, output=output, BLOCK_SIZE=BLOCK_SIZE)
