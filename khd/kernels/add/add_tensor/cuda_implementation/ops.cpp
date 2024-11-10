331b29d7013e2d9faa5085d1da39bfc5dbac73d5
Mayank Mishra <mayank31398@gmail.com>
2024-11-10 18:07:13 -0500

    reformat
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- khd/kernels/add/add_tensor/cuda_implementation/ops.cpp
+++ khd/kernels/add/add_tensor/cuda_implementation/ops.cpp
@@ -1,9 +1,7 @@
 #include <torch/extension.h>
 
-void add_tensor_forward_cuda(const torch::Tensor x,
-                             const torch::Tensor y,
-                             torch::Tensor output,
-                             const int &BLOCK_SIZE);
+void add_tensor_forward_cuda(
+    const torch::Tensor x, const torch::Tensor y, torch::Tensor output, const int &BLOCK_SIZE);
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
     m.def("add_tensor_forward_cuda", &add_tensor_forward_cuda, "Tensor addition forward (CUDA)");
--- khd/kernels/swiglu/cuda_implementation/ops.cpp
+++ khd/kernels/swiglu/cuda_implementation/ops.cpp
@@ -3,11 +3,11 @@
 void swiglu_forward_cuda(torch::Tensor gate, torch::Tensor up, torch::Tensor output, const int BLOCK_SIZE);
 
 void swiglu_backward_cuda(torch::Tensor gate,
-                          torch::Tensor up,
-                          torch::Tensor output_grad,
-                          torch::Tensor gate_grad,
-                          torch::Tensor up_grad,
-                          const int BLOCK_SIZE);
+    torch::Tensor up,
+    torch::Tensor output_grad,
+    torch::Tensor gate_grad,
+    torch::Tensor up_grad,
+    const int BLOCK_SIZE);
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
     m.def("swiglu_forward_cuda", &swiglu_forward_cuda, "SwiGLU forward (CUDA)");
