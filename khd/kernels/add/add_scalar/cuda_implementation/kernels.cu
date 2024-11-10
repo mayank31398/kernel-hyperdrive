d0ee75a6cf7b474c38e80379af6c7b2c7228c77d
Mayank Mishra <mayank31398@gmail.com>
2024-11-10 16:45:15 -0500

    reformat
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- khd/kernels/add/add_scalar/cuda_implementation/kernels.cu
+++ khd/kernels/add/add_scalar/cuda_implementation/kernels.cu
@@ -16,11 +16,8 @@ __global__ void _add_scalar_forward_cuda_kernel(const scalar_t *x,
     const int64_t start = thread_id * vector_instruction_width;
     const int64_t end = (thread_id + 1) * vector_instruction_width - 1; // inclusive of last element
 
-    using dtype = DType<scalar_t>;
-    using T2 = typename dtype::nv_dtype2;
-
     if (start < num_elements && end < num_elements) {
-        const fp32 *x_vec = (fp32 *)&((const fp32_4 *)x)[thread_id];
+        const fp32 *_x = (fp32 *)&((fp32_4 *)x)[thread_id];
 
         fp32 output_buffer[4];
 
@@ -28,45 +25,37 @@ __global__ void _add_scalar_forward_cuda_kernel(const scalar_t *x,
         #pragma unroll
         // clang-format on
         for (int i = 0; i < 4; i++) {
-            if (std::is_same_v<scalar_t, fp32>) {
-                output_buffer[i] = x_vec[i] + y;
-            } else if constexpr (std::is_same_v<scalar_t, c10::Half> || std::is_same_v<scalar_t, c10::BFloat16>) {
-                T2 _x = dtype::reinterpret_32_bits_as_2x16(x_vec[i]);
-
-                fp32_2 _x_upcast = dtype::upcast(_x);
-                _x_upcast = make_float2(_x_upcast.x + y, _x_upcast.y + y);
-
-                _x = dtype::downcast(_x_upcast);
-
-                output_buffer[i] = dtype::reinterpret_2x16_as_32_bits(_x);
+            if constexpr (std::is_same_v<scalar_t, fp32>) {
+                output_buffer[i] = _x[i] + y;
             } else {
-                assert(false && "Function not implemented");
+                fp32_2 _x_upcast = dtype::upcast(dtype::reinterpret_32_bits_as_2x16(_x[i]));
+                _x_upcast = dtype::make2(_x_upcast.x + y, _x_upcast.y + y);
+                output_buffer[i] = dtype::reinterpret_2x16_as_32_bits(dtype::downcast(_x_upcast));
             }
         }
 
-        ((fp32_4 *)output)[thread_id] =
-            make_float4(output_buffer[0], output_buffer[1], output_buffer[2], output_buffer[3]);
+        ((fp32_4 *)output)[thread_id] = DType<fp32>::make4(output_buffer);
     } else if (start < num_elements) {
         // clang-format off
         #pragma unroll
         // clang-format on
-        for (int i = start; i < num_elements; i++) {
+        for (int64_t i = start; i < num_elements; i++) {
             output[i] = x[i] + y;
         }
     }
 }
 
 void add_scalar_forward_cuda(const torch::Tensor x, const float &y, torch::Tensor output, const int &BLOCK_SIZE) {
-    const int num_elements = x.numel();
+    const int64_t num_elements = x.numel();
 
-    AT_DISPATCH_CUSTOM_FLOAT_TYPES(
-        x.scalar_type(), "add_scalar_forward_cuda_kernel", ([&] {
-            const int num_elements_per_thread = get_num_elements_in_vector_dtype<scalar_t, fp32_4>();
+    AT_DISPATCH_CUSTOM_FLOAT_TYPES(x.scalar_type(), "add_scalar_forward_cuda_kernel", ([&] {
+                                       const int vector_instruction_width = sizeof(fp32_4) / sizeof(scalar_t);
 
-            const int num_elements_per_block = BLOCK_SIZE * num_elements_per_thread;
-            const int NUM_BLOCKS = (num_elements + num_elements_per_block - 1) / num_elements_per_block;
+                                       const int num_elements_per_block = BLOCK_SIZE * vector_instruction_width;
+                                       const int NUM_BLOCKS =
+                                           (num_elements + num_elements_per_block - 1) / num_elements_per_block;
 
-            _add_scalar_forward_cuda_kernel<scalar_t>
-                <<<NUM_BLOCKS, BLOCK_SIZE>>>(x.data_ptr<scalar_t>(), y, output.data_ptr<scalar_t>(), num_elements);
-        }));
+                                       _add_scalar_forward_cuda_kernel<scalar_t><<<NUM_BLOCKS, BLOCK_SIZE>>>(
+                                           x.data_ptr<scalar_t>(), y, output.data_ptr<scalar_t>(), num_elements);
+                                   }));
 }
