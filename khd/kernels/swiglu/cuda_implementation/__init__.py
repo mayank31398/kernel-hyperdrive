59cfffc0a1e03e4d01cc853f362381b327c321c3
Mayank Mishra <mayank31398@gmail.com>
2024-11-11 17:46:20 -0500

    merge
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- khd/kernels/swiglu/cuda_implementation/__init__.py
+++ khd/kernels/swiglu/cuda_implementation/__init__.py
@@ -8,15 +8,19 @@ _FORWARD_KERNEL_NAME = "swiglu_forward_cuda"
 _BACKWARD_KERNEL_NAME = "swiglu_backward_cuda"
 
 
-def swiglu_forward_cuda_kernel(gate: torch.Tensor, up: torch.Tensor, output: torch.Tensor, BLOCK_SIZE: int) -> None:
-    KernelRegistry.get_kernel(_FORWARD_KERNEL_NAME)(gate, up, output, BLOCK_SIZE)
+def swiglu_forward_cuda_kernel(
+    gate: torch.Tensor, up: torch.Tensor, output: torch.Tensor, vector_instruction_width: int, BLOCK_SIZE: int
+) -> None:
+    KernelRegistry.get_kernel(_FORWARD_KERNEL_NAME)(gate, up, output, vector_instruction_width, BLOCK_SIZE)
 
 
 @torch.library.custom_op(f"{LIBRARY_NAME}::{_FORWARD_KERNEL_NAME}", mutates_args={"output"})
 def swiglu_forward_cuda_kernel_compileable(
-    gate: torch.Tensor, up: torch.Tensor, output: torch.Tensor, BLOCK_SIZE: int
+    gate: torch.Tensor, up: torch.Tensor, output: torch.Tensor, vector_instruction_width: int, BLOCK_SIZE: int
 ) -> None:
-    swiglu_forward_cuda_kernel(gate=gate, up=up, output=output, BLOCK_SIZE=BLOCK_SIZE)
+    swiglu_forward_cuda_kernel(
+        gate=gate, up=up, output=output, vector_instruction_width=vector_instruction_width, BLOCK_SIZE=BLOCK_SIZE
+    )
 
 
 def swiglu_backward_cuda_kernel(
