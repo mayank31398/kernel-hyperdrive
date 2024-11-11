71ec8961522a07030a493c224c5308227487f0e1
Mayank Mishra <mayank31398@gmail.com>
2024-11-11 01:22:42 -0500

    merge
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- khd/kernels/add/add_tensor/cuda_implementation/kernels.cu
+++ khd/kernels/add/add_tensor/cuda_implementation/kernels.cu
@@ -49,7 +49,7 @@ __global__ void _add_tensor_forward_cuda_kernel(const scalar_t *x,
                     output_vec[thread_id] = dtype::make4(output_buffer);
                 }
             } else {
-                using T2 = DType<scalar_t>;
+                using T2 = dtype::nv_dtype2;
 
                 if constexpr (std::is_same_v<vector_t, fp16_2> || std::is_same_v<vector_t, bf16_2>) {
                     T2 _x = ((vector_t *)x)[thread_id];
