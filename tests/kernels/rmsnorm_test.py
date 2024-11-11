a5d1e5c14cde1a38f084bfa72117719ef4cbf79a
Mayank Mishra <mayank31398@gmail.com>
2024-11-11 17:57:14 -0500

    merge
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- tests/kernels/rmsnorm_test.py
+++ tests/kernels/rmsnorm_test.py
@@ -15,34 +15,36 @@ _EPSILON = 1e-5
 class RMSNormTest(TestCommons):
     @parameterized.expand(
         TestCommons.make_args_matrix(
-            TestCommons.get_2d_tensor_sizes(),
-            [torch.device("cuda")],
-            TestCommons.get_dtypes(),
-            [
-                partial(
-                    rmsnorm_khd,
-                    eps=_EPSILON,
-                    kernel_backend=KernelBackend.triton,
-                    BLOCK_SIZE_B=32,
-                    BLOCK_SIZE_H=32,
-                ),
-                torch.compile(
-                    partial(
-                        rmsnorm_khd,
-                        eps=_EPSILON,
-                        kernel_backend=KernelBackend.triton,
-                        BLOCK_SIZE_B=32,
-                        BLOCK_SIZE_H=32,
-                    )
-                ),
-            ],
+            TestCommons.get_2d_tensor_sizes(),  # size
+            [torch.device("cuda")],  # device
+            TestCommons.get_dtypes(),  # dtype
+            [KernelBackend.triton],  # kernel_backend
+            [32],  # BLOCK_SIZE_B
+            [32],  # BLOCK_SIZE_H
+            [rmsnorm_khd, torch.compile(rmsnorm_khd)],  # function
         )
     )
-    def test_rmsnorm(self, size: tuple[int], device: torch.device, dtype: torch.dtype, function: Callable) -> None:
+    def test_rmsnorm(
+        self,
+        size: tuple[int],
+        device: torch.device,
+        dtype: torch.dtype,
+        kernel_backend: KernelBackend,
+        BLOCK_SIZE_B: int,
+        BLOCK_SIZE_H: int,
+        function: Callable,
+    ) -> None:
         x_kernel, x_expected = self.get_random_duplicated_tensors(size, device=device, dtype=dtype)
         weight_kernel, weight_expected = self.get_random_duplicated_tensors(size[-1], device=device, dtype=dtype)
 
-        z_kernel = function(x=x_kernel, weight=weight_kernel, eps=_EPSILON)
+        z_kernel = function(
+            x=x_kernel,
+            weight=weight_kernel,
+            eps=_EPSILON,
+            kernel_backend=kernel_backend,
+            BLOCK_SIZE_B=BLOCK_SIZE_B,
+            BLOCK_SIZE_H=BLOCK_SIZE_H,
+        )
         z_expected = rmsnorm_torch(x=x_expected, weight=weight_expected, eps=_EPSILON)
 
         # z_kernel.mean().backward()
