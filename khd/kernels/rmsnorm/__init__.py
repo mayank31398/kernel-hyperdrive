f6fb1c031ddf88b89d64a1fbd4e196faf36f6df4
Mayank Mishra <mayank31398@gmail.com>
2024-11-12 11:08:58 -0500

    rms
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- khd/kernels/rmsnorm/__init__.py
+++ khd/kernels/rmsnorm/__init__.py
@@ -3,9 +3,9 @@ import triton
 
 from ...constants import BLOCK_SIZES_POWERS_OF_2
 from ...enums import KernelBackend
-from ...utils import CutoTuneParameter, cutotune, get_cartesian_product_cutotune_configs
+from ...utils import CutoTuneParameter, cutotune, ensure_same_strides, get_cartesian_product_cutotune_configs
 from .torch_implementation import rmsnorm_torch
-from .triton_implementation import rmsnorm_forward_triton_kernel
+from .triton_implementation import rmsnorm_backward_triton_kernel, rmsnorm_forward_triton_kernel
 
 
 class _RMSNorm_KHD(torch.autograd.Function):
@@ -36,6 +36,7 @@ class _RMSNorm_KHD(torch.autograd.Function):
         if weight is not None:
             assert weight.dim() == 1, "weight should be 1D"
             assert weight.size(-1) == x.size(-1), "hidden size for x and weight tensor is different"
+            assert weight.type() == x.type(), "tensors weight and y should have same dtype"
 
             weight = weight.contiguous()
 
@@ -46,13 +47,13 @@ class _RMSNorm_KHD(torch.autograd.Function):
 
         output = torch.empty_like(x)
         rmsnorm_denominator = (
-            None if memory_efficient else torch.empty(num_elements, 1, device=x.device, dtype=torch.float32)
+            None if memory_efficient else torch.empty(num_elements, 1, device=x.get_device(), dtype=torch.float32)
         )
 
         if kernel_backend == KernelBackend.triton:
             grid = lambda meta: (triton.cdiv(num_elements, meta["BLOCK_SIZE_B"]),)
 
-            with torch.device(x.device):
+            with torch.device(x.get_device()):
                 rmsnorm_forward_triton_kernel[grid](
                     x_ptr=x,
                     x_stride_b=x_view.stride(0),
@@ -73,9 +74,62 @@ class _RMSNorm_KHD(torch.autograd.Function):
         else:
             raise ValueError(f"unexpected kernel_backend ({kernel_backend})")
 
-        ctx.save_for_backward(rmsnorm_denominator)
+        ctx.memory_efficient = memory_efficient
+        ctx.has_weight = weight is not None
+
+        if memory_efficient:
+            ctx.save_for_backward(x)
+        else:
+            ctx.save_for_backward(x, rmsnorm_denominator)
 
         return output
+
+    @staticmethod
+    def backward(ctx, output_grad: torch.Tensor) -> tuple[torch.Tensor | None]:
+        memory_efficient = ctx.memory_efficient
+        kernel_backend = ctx.kernel_backend_backward
+        has_weight = ctx.has_weight
+
+        if memory_efficient:
+            (x,) = ctx.saved_tensors
+        else:
+            x, rmsnorm_denominator = ctx.saved_tensors
+
+        x, output_grad = ensure_same_strides(x, output_grad)
+
+        hidden_size = x.size(-1)
+        num_elements = x.numel() // hidden_size
+
+        x_view = x.view(-1, hidden_size)
+        output_grad_view = output_grad.view(-1, hidden_size)
+
+        x_grad = torch.empty_like(x)
+        weight_grad = torch.empty(hidden_size, x.dtype, device=x.device)
+
+        if kernel_backend == KernelBackend.triton:
+            grid = lambda meta: (triton.cdiv(num_elements, meta["BLOCK_SIZE_B"]),)
+
+            with torch.device(x.device):
+                rmsnorm_backward_triton_kernel[grid](
+                    x_ptr=x,
+                    x_stride_b=x_view.stride(0),
+                    x_stride_h=x_view.stride(1),
+                    has_weight=has_weight,
+                    output_grad_ptr=output_grad_view,
+                    output_grad_stride_b=output_grad_view.stride(0),
+                    output_grad_stride_h=output_grad_view.stride(1),
+                    eps=eps,
+                    memory_efficient=memory_efficient,
+                    rmsnorm_denominator_ptr=rmsnorm_denominator,
+                    B=num_elements,
+                    H=hidden_size,
+                    BLOCK_SIZE_B=BLOCK_SIZE_B,
+                    BLOCK_SIZE_H=BLOCK_SIZE_H,
+                )
+        else:
+            raise ValueError(f"unexpected kernel_backend ({kernel_backend})")
+
+        return x_grad, weight_grad, None, None, None, None, None
 
 
 def rmsnorm_khd(
