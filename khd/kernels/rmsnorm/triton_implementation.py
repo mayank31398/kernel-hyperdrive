2f75a2ab75c30632a1f99172ca9d8eedc914662c
Mayank Mishra <mayank31398@gmail.com>
2024-11-12 03:06:09 -0500

    rms
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- khd/kernels/rmsnorm/triton_implementation.py
+++ khd/kernels/rmsnorm/triton_implementation.py
@@ -88,7 +88,6 @@ def rmsnorm_backward_triton_kernel(
     x_stride_b,
     x_stride_h,
     has_weight: tl.constexpr,
-    weight_ptr,
     output_grad_ptr,
     output_grad_stride_b,
     output_grad_stride_h,
@@ -101,7 +100,9 @@ def rmsnorm_backward_triton_kernel(
     BLOCK_SIZE_H: tl.constexpr,
 ):
     num_iterations_h = tl.cdiv(H, BLOCK_SIZE_H)
-    weight_grad = tl.zeros((H,), dtype=tl.float32)
+
+    if has_weight:
+        weight_grad = tl.zeros((H,), dtype=tl.float32)
 
     for pid_b in range(BLOCK_SIZE_B):
         block_start_b = pid_b * BLOCK_SIZE_B
@@ -110,10 +111,12 @@ def rmsnorm_backward_triton_kernel(
 
         # when num_iterations_h is 1, we can optimize further
         if num_iterations_h == 1:
-            indices_h = tl.arange(0, H)
+            indices_h = tl.arange(0, BLOCK_SIZE_H)
+            mask_h = indices_h < H
+            mask_bh = mask_b[:, None] & mask_h[None, :]
 
             x_ptrs = x_ptr + indices_b[:, None] * x_stride_b + indices_h[None, :] * x_stride_h
-            x = tl.load(x_ptrs, mask=mask_b[:, None]).to(tl.float32)
+            x = tl.load(x_ptrs, mask=mask_bh).to(tl.float32)
 
             denominator = tl.sum(x * x, axis=1, keep_dims=True)
             denominator = tl.rsqrt((denominator / H) + eps)
@@ -122,15 +125,23 @@ def rmsnorm_backward_triton_kernel(
             output_grad_ptrs = (
                 output_grad_ptr + indices_b[:, None] * output_grad_stride_b + indices_h[None, :] * output_grad_stride_h
             )
-            output_grad = tl.load(output_grad_ptrs, mask=mask_b[:, None]).to(tl.float32)
+            output_grad = tl.load(output_grad_ptrs, mask=mask_bh).to(tl.float32)
 
             if has_weight:
                 weight_grad += tl.sum(y * output_grad, axis=0)
+
+            x_grad = denominator * (1 - y * y)
+
+            x_grad_ptrs = x_grad_ptr + indices_b[:, None] * x_stride_b + indices_h[None, :] * x_stride_h
+            tl.load(x_grad_ptrs, x_grad, mask=mask_bh)
         else:
             pass
 
-    if num_iterations_h == 1:
-        weight_grad_ptrs = weight_grad_ptr + tl.arange(0, H)
-        tl.store(weight_grad_ptrs, weight_grad)
-    else:
-        pass
+    if has_weight:
+        if num_iterations_h == 1:
+            indices_h = tl.arange(0, BLOCK_SIZE_H)
+            mask_h = indices_h < H
+
+            tl.store(weight_grad_ptr + indices_h, weight_grad, mask=mask_h)
+        else:
+            pass
