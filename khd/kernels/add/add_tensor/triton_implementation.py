4bf9e15893fe285f24a7e39e1a2f7bda0bdda732
Mayank Mishra <mayank31398@gmail.com>
2024-11-13 09:34:33 -0500

    mask
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- khd/kernels/add/add_tensor/triton_implementation.py
+++ khd/kernels/add/add_tensor/triton_implementation.py
@@ -8,11 +8,10 @@ def add_tensor_forward_triton_kernel(x_ptr, y_ptr, output_ptr, num_elements, BLO
     num_blocks = tl.num_programs(axis=0)
     is_last_block = pid == num_blocks - 1
 
-    block_start = pid * BLOCK_SIZE
-    indices = block_start + tl.arange(0, BLOCK_SIZE)
-    mask = indices < num_elements
+    indices = pid * BLOCK_SIZE + tl.arange(0, BLOCK_SIZE)
 
     if is_last_block:
+        mask = indices < num_elements
         x = tl.load(x_ptr + indices, mask=mask)
         y = tl.load(y_ptr + indices, mask=mask)
     else:
