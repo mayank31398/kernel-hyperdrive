57dd1a2a34b74fac47a1c9ecd28c345f036266f8
Mayank Mishra <mayank31398@gmail.com>
2024-11-13 09:57:15 -0500

    rms
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- khd/kernels/add/add_scalar/triton_implementation.py
+++ khd/kernels/add/add_scalar/triton_implementation.py
@@ -3,6 +3,12 @@ import triton.language as tl
 
 
 @triton.jit
+def _add(x_ptr, y, output_ptr, indices, mask):
+    x = tl.load(x_ptr + indices, mask=mask)
+    tl.store(output_ptr + indices, x + y, mask=mask)
+
+
+@triton.jit
 def add_scalar_forward_triton_kernel(x_ptr, y, output_ptr, num_elements, BLOCK_SIZE: tl.constexpr):
     pid = tl.program_id(axis=0)
     num_blocks = tl.num_programs(axis=0)
@@ -12,9 +18,6 @@ def add_scalar_forward_triton_kernel(x_ptr, y, output_ptr, num_elements, BLOCK_S
 
     if is_last_block:
         mask = indices < num_elements
-
-        x = tl.load(x_ptr + indices, mask=mask)
-        tl.store(output_ptr + indices, x + y, mask=mask)
+        _add(x_ptr=x_ptr, y=y, output_ptr=output_ptr, indices=indices, mask=mask)
     else:
-        x = tl.load(x_ptr + indices)
-        tl.store(output_ptr + indices, x + y)
+        _add(x_ptr=x_ptr, y=y, output_ptr=output_ptr, indices=indices, mask=None)
