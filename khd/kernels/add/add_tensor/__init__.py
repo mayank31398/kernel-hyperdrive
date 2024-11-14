c091cafe7d0cca4ab6763340b349f3057c210ccb
Mayank Mishra <mayank31398@gmail.com>
2024-11-14 16:44:57 -0500

    fullgraph
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- khd/kernels/add/add_tensor/__init__.py
+++ khd/kernels/add/add_tensor/__init__.py
@@ -9,72 +9,74 @@ from .torch_implementation import add_tensor_torch
 from .triton_implementation import add_tensor_forward_triton_kernel
 
 
-class _AddTensor_KHD(torch.autograd.Function):
-    @cutotune(
-        configs=(
-            get_cartesian_product_cutotune_configs(
-                kernel_backend=[KernelBackend.cuda],
-                vector_instruction_width=[1, 2, 4],
-                BLOCK_SIZE=BLOCK_SIZES_POWERS_OF_2,
-            )
-            if torch.cuda.is_available()
-            else []
+@cutotune(
+    configs=(
+        get_cartesian_product_cutotune_configs(
+            kernel_backend=[KernelBackend.cuda],
+            vector_instruction_width=[1, 2, 4],
+            BLOCK_SIZE=BLOCK_SIZES_POWERS_OF_2,
         )
-        + (
-            get_cartesian_product_cutotune_configs(
-                kernel_backend=[KernelBackend.cuda],
-                vector_instruction_width=[8],
-                BLOCK_SIZE=BLOCK_SIZES_POWERS_OF_2,
-                condition=lambda **kwargs: kwargs["x"].dtype in [torch.float16, torch.bfloat16],
-            )
-            if torch.cuda.is_available()
-            else []
-        )
-        + get_cartesian_product_cutotune_configs(
-            kernel_backend=[KernelBackend.triton], vector_instruction_width=[None], BLOCK_SIZE=BLOCK_SIZES_POWERS_OF_2
-        ),
-        triggers={"x.dtype"},
+        if torch.cuda.is_available()
+        else []
     )
-    def _forward(
-        ctx,
-        x: torch.Tensor,
-        y: torch.Tensor,
-        kernel_backend: KernelBackend | CutoTuneParameter,
-        vector_instruction_width: int | CutoTuneParameter,
-        BLOCK_SIZE: int | CutoTuneParameter,
-    ) -> torch.Tensor:
-        assert x.size() == y.size(), "tensors x and y should have same shape"
-        assert x.type() == y.type(), "tensors x and y should have same dtype"
+    + (
+        get_cartesian_product_cutotune_configs(
+            kernel_backend=[KernelBackend.cuda],
+            vector_instruction_width=[8],
+            BLOCK_SIZE=BLOCK_SIZES_POWERS_OF_2,
+            condition=lambda **kwargs: kwargs["x"].dtype in [torch.float16, torch.bfloat16],
+        )
+        if torch.cuda.is_available()
+        else []
+    )
+    + get_cartesian_product_cutotune_configs(
+        kernel_backend=[KernelBackend.triton], vector_instruction_width=[None], BLOCK_SIZE=BLOCK_SIZES_POWERS_OF_2
+    ),
+    triggers={"x.dtype"},
+)
+def _forward(
+    ctx,
+    x: torch.Tensor,
+    y: torch.Tensor,
+    kernel_backend: KernelBackend | CutoTuneParameter,
+    vector_instruction_width: int | CutoTuneParameter,
+    BLOCK_SIZE: int | CutoTuneParameter,
+) -> torch.Tensor:
+    assert x.size() == y.size(), "tensors x and y should have same shape"
+    assert x.type() == y.type(), "tensors x and y should have same dtype"
 
-        x, y = ensure_same_strides(x, y)
-        output = torch.empty_like(x)
+    x, y = ensure_same_strides(x, y)
+    output = torch.empty_like(x)
 
-        if kernel_backend == KernelBackend.cuda:
-            assert x.is_cuda, "tensor x is not on GPU"
-            assert y.is_cuda, "tensor y is not on GPU"
+    if kernel_backend == KernelBackend.cuda:
+        assert x.is_cuda, "tensor x is not on GPU"
+        assert y.is_cuda, "tensor y is not on GPU"
 
-            if torch.compiler.is_compiling():
-                add_tensor_forward_cuda_kernel_compileable(
-                    x=x, y=y, output=output, vector_instruction_width=vector_instruction_width, BLOCK_SIZE=BLOCK_SIZE
-                )
-            else:
-                add_tensor_forward_cuda_kernel(
-                    x=x, y=y, output=output, vector_instruction_width=vector_instruction_width, BLOCK_SIZE=BLOCK_SIZE
-                )
-        elif kernel_backend == KernelBackend.triton:
-            assert vector_instruction_width is None
-
-            num_elements = x.numel()
-            grid = lambda meta: (triton.cdiv(num_elements, meta["BLOCK_SIZE"]),)
-
-            with torch.device(x.device):
-                add_tensor_forward_triton_kernel[grid](
-                    x_ptr=x, y_ptr=y, output_ptr=output, num_elements=num_elements, BLOCK_SIZE=BLOCK_SIZE
-                )
+        if torch.compiler.is_compiling():
+            add_tensor_forward_cuda_kernel_compileable(
+                x=x, y=y, output=output, vector_instruction_width=vector_instruction_width, BLOCK_SIZE=BLOCK_SIZE
+            )
         else:
-            raise ValueError(f"unexpected kernel_backend ({kernel_backend})")
+            add_tensor_forward_cuda_kernel(
+                x=x, y=y, output=output, vector_instruction_width=vector_instruction_width, BLOCK_SIZE=BLOCK_SIZE
+            )
+    elif kernel_backend == KernelBackend.triton:
+        assert vector_instruction_width is None
 
-        return output
+        num_elements = x.numel()
+        grid = lambda meta: (triton.cdiv(num_elements, meta["BLOCK_SIZE"]),)
+
+        with torch.device(x.device):
+            add_tensor_forward_triton_kernel[grid](
+                x_ptr=x, y_ptr=y, output_ptr=output, num_elements=num_elements, BLOCK_SIZE=BLOCK_SIZE
+            )
+    else:
+        raise ValueError(f"unexpected kernel_backend ({kernel_backend})")
+
+    return output
+
+
+class _AddTensor_KHD(torch.autograd.Function):
 
     @staticmethod
     def forward(
@@ -85,7 +87,7 @@ class _AddTensor_KHD(torch.autograd.Function):
         vector_instruction_width: int | CutoTuneParameter,
         BLOCK_SIZE: int | CutoTuneParameter,
     ) -> torch.Tensor:
-        return _AddTensor_KHD._forward(
+        return _forward(
             ctx=ctx,
             x=x,
             y=y,
