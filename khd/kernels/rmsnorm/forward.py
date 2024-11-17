c7fbbced6970743fc641f04c31f4857d0cd41b6f
Mayank Mishra <mayank31398@gmail.com>
2024-11-17 10:34:59 -0500

    configs
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- khd/kernels/rmsnorm/forward.py
+++ khd/kernels/rmsnorm/forward.py
@@ -1,30 +1,10 @@
 import torch
 import triton
 
-from ...constants import BLOCK_SIZES_POWERS_OF_2
 from ...enums import KernelBackend
-from ...utils import CutoTuneConfig, CutoTuneParameter, cutotune
+from ...utils import CutoTuneParameter, cutotune
+from .configs import _get_cutotune_configs
 from .triton_implementation import rmsnorm_forward_triton_kernel
-
-
-def _get_cutotune_configs() -> list[CutoTuneConfig]:
-    configs = []
-    for BLOCK_SIZE_H in BLOCK_SIZES_POWERS_OF_2:
-        for BLOCK_SIZE_B in [1, 2, 4, 8, 16, 32] + BLOCK_SIZES_POWERS_OF_2:
-            # we only use realistic configs where the block has between 64 and 64k elements
-            if 64 < BLOCK_SIZE_B * BLOCK_SIZE_H <= 65536:
-                configs.append(
-                    CutoTuneConfig(
-                        config={
-                            "kernel_backend": KernelBackend.triton,
-                            "BLOCK_SIZE_B": BLOCK_SIZE_B,
-                            "BLOCK_SIZE_H": BLOCK_SIZE_H,
-                        },
-                        condition=lambda **kwargs: kwargs["x"].size(-1) <= kwargs["BLOCK_SIZE_H"],
-                    )
-                )
-
-    return configs
 
 
 @cutotune(configs=_get_cutotune_configs(), triggers={"x.dtype", "x.size(-1)"})
