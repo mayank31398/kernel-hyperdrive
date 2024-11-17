2ac569e6c95bd34cada10a64edb5a0c1fc11c98b
Mayank Mishra <mayank31398@gmail.com>
2024-11-17 03:00:31 -0500

    add comment
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- khd/kernels/rmsnorm/forward.py
+++ khd/kernels/rmsnorm/forward.py
@@ -11,6 +11,7 @@ def _get_cutotune_configs() -> list[CutoTuneConfig]:
     configs = []
     for BLOCK_SIZE_H in BLOCK_SIZES_POWERS_OF_2:
         for BLOCK_SIZE_B in [1, 2, 4, 8, 16, 32] + BLOCK_SIZES_POWERS_OF_2:
+            # we only use realistic configs where the block has between 64 and 64k elements
             if 64 < BLOCK_SIZE_B * BLOCK_SIZE_H <= 65536:
                 configs.append(
                     CutoTuneConfig(
