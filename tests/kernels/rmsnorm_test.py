8d808fc32d0401d661dfbf56b379d02d07696a86
Mayank Mishra <mayank31398@gmail.com>
2024-11-19 02:22:42 -0500

    sms
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- tests/kernels/rmsnorm_test.py
+++ tests/kernels/rmsnorm_test.py
@@ -39,9 +39,9 @@ class RMSNormTest(TestCommons):
         z_kernel = function(x=x_kernel, weight=weight_kernel, eps=_EPSILON, memory_efficient=memory_efficient)
         z_expected = rmsnorm_torch(x=x_expected, weight=weight_expected, eps=_EPSILON)
 
-        # z_kernel.sum().backward()
-        # z_expected.sum().backward()
+        z_kernel.sum().backward()
+        z_expected.sum().backward()
 
         self.assert_equal_tensors(z_kernel, z_expected, False, atol_float16=8e-3, rtol_float16=0)
-        # self.assert_equal_tensors(x_kernel.grad, x_expected.grad, False)
+        self.assert_equal_tensors(x_kernel.grad, x_expected.grad, False, atol_float16=0.07, rtol_float16=0)
         # self.assert_equal_tensors(weight_kernel.grad, weight_expected.grad, False, atol_float32=8.5e-5, rtol_float32=0)
