1a38676371d72af44a23de4c59e53612ae99e394
Mayank Mishra <mayank31398@gmail.com>
2024-11-22 03:56:17 -0500

    freq
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- khd/kernels/contiguous_count/triton_implementation.py
+++ khd/kernels/contiguous_count/triton_implementation.py
@@ -12,23 +12,20 @@ def contiguous_count_triton_kernel(
     num_elements_per_program = tl.cdiv(B, num_programs)
     num_loops = tl.cdiv(num_elements_per_program, BLOCK_SIZE_B)
 
-    counts = tl.zeros((BLOCK_SIZE_C,), dtype=tl.int32)
-
     indices_c = tl.arange(0, BLOCK_SIZE_C)
     mask_c = indices_c < C
 
-    offset = pid * num_elements_per_program
+    counts = tl.zeros((BLOCK_SIZE_C,), dtype=tl.int32)
 
     for i in range(num_loops):
-        indices_b = offset + i * BLOCK_SIZE_B + tl.arange(0, BLOCK_SIZE_B)
+        start = pid * num_elements_per_program + i * BLOCK_SIZE_B
+        indices_b = start + tl.arange(0, BLOCK_SIZE_B)
         mask_b = indices_b < B
 
         x = tl.load(x_ptr + indices_b, mask=mask_b)
 
         equal = x[:, None] == indices_c[None, :]
         counts += tl.sum(equal, axis=0)
-
-    tl.device_print("c", counts)
 
     output_ptrs = output_ptr + pid * output_stride_b + indices_c
     tl.store(output_ptrs, counts, mask=mask_c)
