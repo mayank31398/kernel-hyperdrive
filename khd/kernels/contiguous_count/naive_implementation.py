6d3fb72fdd3d3992379192915fb4552a60f30865
Mayank Mishra <mayank31398@gmail.com>
2024-11-22 06:33:48 -0500

    freq
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- khd/kernels/contiguous_count/naive_implementation.py
+++ khd/kernels/contiguous_count/naive_implementation.py
@@ -7,18 +7,23 @@ def contiguous_count_naive_kernel(
     x: torch.Tensor, output: torch.Tensor, num_programs: int, B: int, C: int, BLOCK_SIZE_B: int, BLOCK_SIZE_C: int
 ) -> None:
     num_elements_per_program = ceil_divide(B, num_programs)
-    num_loops = ceil_divide(num_elements_per_program, BLOCK_SIZE_B)
-
-    max_c = min(C, BLOCK_SIZE_C)
-    indices_c = torch.arange(0, max_c, device=x.device)
+    indices_c = torch.arange(0, BLOCK_SIZE_C, device=x.device)
 
     for pid in range(num_programs):
-        counts = torch.zeros(max_c, dtype=torch.int32, device=x.device)
+        program_start = pid * num_elements_per_program
+        program_end = min(program_start + num_elements_per_program, B)
+        num_elements_in_current_program = program_end - program_start
+
+        num_loops = ceil_divide(num_elements_in_current_program, BLOCK_SIZE_B)
+        counts = torch.zeros(BLOCK_SIZE_C, dtype=torch.int32, device=x.device)
 
         for i in range(num_loops):
-            start = pid * num_elements_per_program + i * BLOCK_SIZE_B
+            start = program_start + i * BLOCK_SIZE_B
             end = min(start + BLOCK_SIZE_B, B)
 
-            counts += (x[start:end].unsqueeze(1) == indices_c.unsqueeze(0)).sum(dim=0)
+            x_ = x[start:end].unsqueeze(1).repeat(1, BLOCK_SIZE_C)
+            indices_c_ = indices_c.unsqueeze(0).repeat(end - start, 1)
 
-        output[pid, :max_c] = counts
+            counts += (x_ == indices_c_).sum(dim=0)
+
+        output[pid] = counts[:C]
