63f3a25e39a70f50fe81b1a3c3dd74436b4f30c5
Mayank Mishra <mayank31398@gmail.com>
2024-11-22 19:40:41 -0500

    drop bincount
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- tests/kernels/contiguous_count_test.py
+++ tests/kernels/contiguous_count_test.py
@@ -32,6 +32,6 @@ class ContiguousCountTest(TestCommons):
         x = torch.randint(0, MAX_EXPERTS, size, device=device, dtype=torch.long)
 
         z_kernel = function(x=x, start=0, end=MAX_EXPERTS, kernel_backend=kernel_backend, BLOCK_SIZE_B=BLOCK_SIZE_B)
-        z_expected = x.bincount(minlength=MAX_EXPERTS)
+        z_expected = x.view(-1).bincount(minlength=MAX_EXPERTS)
 
         self.assert_equal_tensors(z_kernel, z_expected, True)
