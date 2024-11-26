917cf6fe796e89ae97abd3282ff00fade88a9767
Mayank Mishra <mayank31398@gmail.com>
2024-11-26 14:21:26 -0500

    ensure_contiguous
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/rmsnorm/__init__.py
+++ cute_kernels/kernels/rmsnorm/__init__.py
@@ -25,15 +25,10 @@ class _RMSNorm_Cute(torch.autograd.Function):
     ) -> torch.Tensor:
         assert x.dim() > 1, "x should have more than 1 dimensions"
 
-        if x.stride(-1) != 1:
-            x = x.contiguous()
-
         if weight is not None:
             assert weight.dim() == 1, "weight should be 1D"
             assert weight.size(-1) == x.size(-1), "hidden size for x and weight tensor is different"
             assert weight.type() == x.type(), "tensors weight and y should have same dtype"
-
-            weight = weight.contiguous()
 
         output, rmsnorm_denominator = _forward(
             x=x,
