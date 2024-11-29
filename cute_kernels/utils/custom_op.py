c11a2946527940fed6fe70b6ab97c2fc0bdf7707
Mayank Mishra <mayank31398@gmail.com>
2024-11-29 18:13:34 -0500

    placeholder
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/utils/custom_op.py
+++ cute_kernels/utils/custom_op.py
@@ -3,29 +3,33 @@ from typing import Callable, Iterable, Sequence
 import torch
 
 
+def _dispatch(func: Callable, compileable_fn: Callable, *args, **kwargs):
+    if torch.compiler.is_compiling():
+        output = compileable_fn(*args, **kwargs)
+    else:
+        output = func(*args, **kwargs)
+
+    return output
+
+
 def torch_custom_op(
-    name: str,
-    fn: Callable | None = None,
-    /,
-    *,
-    mutates_args: str | Iterable[str],
+    name: str = None,
+    mutates_args: str | Iterable[str] = None,
     device_types: str | Sequence[str] | None = None,
     schema: str | None = None,
-    fake_fn: Callable | None = None,
+    fake_func: Callable | None = None,
 ) -> Callable:
-    compileable_fn = torch.library.custom_op(
-        name, fn, mutates_args=mutates_args, device_types=device_types, schema=schema
-    )
+    def _inner(func: Callable):
+        compileable_func = torch.library.custom_op(
+            name, func, mutates_args=mutates_args, device_types=device_types, schema=schema
+        )
 
-    if fake_fn is not None:
-        compileable_fn.register_fake(fake_fn)
+        if fake_func is not None:
+            compileable_func.register_fake(fake_func)
 
-    def inner(*args, **kwargs):
-        if torch.compiler.is_compiling():
-            output = compileable_fn(*args, **kwargs)
-        else:
-            output = fn(*args, **kwargs)
+        def _run(*args, **kwargs):
+            return _dispatch(func, compileable_func, *args, **kwargs)
 
-        return output
+        return _run
 
-    return inner
+    return _inner
