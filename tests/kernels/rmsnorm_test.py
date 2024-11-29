17975da2e3661eb584cb58f033f1b85621b38597
Mayank Mishra <mayank31398@gmail.com>
2024-11-29 15:43:20 -0500

    backward
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- tests/kernels/rmsnorm_test.py
+++ tests/kernels/rmsnorm_test.py
@@ -50,7 +50,7 @@ class RMSNormTest(TestCommons):
         z_expected.sum().backward()
 
         self.assert_equal_tensors(z_kernel, z_expected, False, atol_float16=8e-3, rtol_float16=0)
-        self.assert_equal_tensors(x_kernel.grad, x_expected.grad, False, atol_float16=7e-2, rtol_float16=0)
+        self.assert_equal_tensors(x_kernel.grad, x_expected.grad, False, atol_float16=9e-2, rtol_float16=0)
 
         if has_weight:
             self.assert_equal_tensors(
