72dd36ecdd2a8ead2328e9d072174be1cc3a8c12
Mayank Mishra <mayank31398@gmail.com>
2024-11-28 19:52:28 -0500

    swiglu readme
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

---
+++ cute_kernels/kernels/swiglu/triton_implementation/__init__.py
@@ -0,0 +1,2 @@
+from .backward_kernels import swiglu_backward_triton_kernel
+from .forward_kernels import swiglu_forward_triton_kernel
--- cute_kernels/kernels/swiglu/triton_implementation/backward_kernels.py
+++ cute_kernels/kernels/swiglu/triton_implementation/backward_kernels.py
@@ -3,21 +3,6 @@ import triton.language as tl
 
 
 @triton.jit
-def swiglu_forward_triton_kernel(gate_ptr, up_ptr, output_ptr, num_elements, BLOCK_SIZE: tl.constexpr):
-    pid = tl.program_id(axis=0)
-
-    indices = pid * BLOCK_SIZE + tl.arange(0, BLOCK_SIZE)
-    mask = indices < num_elements
-
-    gate = tl.load(gate_ptr + indices, mask=mask).to(tl.float32)
-    up = tl.load(up_ptr + indices, mask=mask)
-
-    output = up * gate * tl.sigmoid(gate)
-
-    tl.store(output_ptr + indices, output, mask=mask)
-
-
-@triton.jit
 def swiglu_backward_triton_kernel(
     gate_ptr, up_ptr, output_grad_ptr, gate_grad_ptr, up_grad_ptr, num_elements, BLOCK_SIZE: tl.constexpr
 ):
