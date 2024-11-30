70b5489451e6f47b02c2b5a0d80d92e4d0d487c2
Mayank Mishra <mayank31398@gmail.com>
2024-11-29 22:37:18 -0500

    embedding
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

---
+++ cute_kernels/kernels/embedding/cuda_implementation/ops.cpp
@@ -0,0 +1,12 @@
+#include <torch/extension.h>
+
+void embedding_forward_cuda(const torch::Tensor &input_ids,
+                            const torch::Tensor &weight,
+                            torch::Tensor output,
+                            const int &vector_instruction_width,
+                            const int &BLOCK_SIZE_B,
+                            const int &BLOCK_SIZE_H);
+
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
+    m.def("embedding_forward_cuda", &embedding_forward_cuda, "Embedding forward (CUDA)");
+}
