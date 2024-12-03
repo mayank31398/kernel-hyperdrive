7c5e4a0fb2ec59ac160fe65ab19a16867513c544
Mayank Mishra <mayank31398@gmail.com>
2024-12-03 18:42:04 -0500

    move
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/include/dtypes/fp32.h
+++ cute_kernels/include/dtypes/fp32.h
@@ -15,6 +15,26 @@ struct DType<fp32> {
     using nv_dtype2 = fp32_2;
     using nv_dtype4 = fp32_4;
 
+    inline __device__ static nv_dtype2 reinterpret_64_bits_as_2x32(const fp64 &value) {
+        auto [left_int, right_int] = split_fp64_into_32_bits(value);
+
+        nv_dtype left = __uint_as_float(left_int);
+        nv_dtype right = __uint_as_float(right_int);
+
+        return make_float2(lower_half, upper_half);
+    }
+
+    inline __device__ static fp64 reinterpret_2x32_as_64_bits(const nv_dtype &lower_half, const nv_dtype &upper_half) {
+        uint32 lower_32 = __float_as_uint(lower_half);
+        uint32 upper_32 = __float_as_uint(upper_half);
+
+        return get_fp64_from_upper_and_lower_32_bits(upper_32, lower_32);
+    }
+
+    inline __device__ static fp32 reinterpret_2x32_as_64_bits(const nv_dtype2 &value) {
+        return reinterpret_2x32_as_64_bits(value.x, value.y);
+    }
+
     inline __device__ static fp32 upcast(const nv_dtype &value) { return value; }
     inline __device__ static fp32_2 upcast(const nv_dtype2 &value) { return value; }
     inline __device__ static fp32_4 upcast(const nv_dtype4 &value) { return value; }
