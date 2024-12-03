162cb9363bd57ca3cb791f55916a6fe716419393
Mayank Mishra <mayank31398@gmail.com>
2024-12-03 16:03:31 -0500

    move
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/add/add_scalar/cuda_implementation/kernels_forward.cu
+++ cute_kernels/kernels/add/add_scalar/cuda_implementation/kernels_forward.cu
@@ -3,7 +3,7 @@
 #include <cuda_runtime.h>
 #include <torch/extension.h>
 
-#include "../../../../include/dtypes.h"
+#include "../../../../include/dtypes/all.h"
 #include "../../../../include/threads.h"
 
 template <typename scalar_t, typename vector_t>
