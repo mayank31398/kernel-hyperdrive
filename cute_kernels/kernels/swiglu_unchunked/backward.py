6e8546cc662b72fdf4352cf3469c4bfeeb857b25
Mayank Mishra <mayank31398@gmail.com>
2024-12-11 04:00:00 -0500

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/swiglu_unchunked/backward.py
+++ cute_kernels/kernels/swiglu_unchunked/backward.py
@@ -2,7 +2,7 @@ import torch
 
 from ...constants import COMMON_TRITON_BLOCK_SIZES_POWERS_OF_2
 from ...enums import KernelBackend
-from ...utils import CutoTuneConfig, ceil_divide, cutotune, get_cartesian_product_cutotune_configs
+from ...utils import CutoTuneConfig, ceil_divide, cutotune, divide_if_divisible, get_cartesian_product_cutotune_configs
 from .triton_implementation import swiglu_unchunked_backward_triton_kernel
 
 
@@ -11,28 +11,27 @@ from .triton_implementation import swiglu_unchunked_backward_triton_kernel
         kernel_backend=[KernelBackend.triton], BLOCK_SIZE=COMMON_TRITON_BLOCK_SIZES_POWERS_OF_2
     ),
     default_config=CutoTuneConfig({"kernel_backend": KernelBackend.triton, "BLOCK_SIZE": 1024}),
-    triggers={"gate.dtype"},
+    triggers={"x.dtype"},
 )
 def _backward(
-    gate: torch.Tensor,
-    up: torch.Tensor,
+    x: torch.Tensor,
     output_grad: torch.Tensor,
     kernel_backend: KernelBackend,
     BLOCK_SIZE: int,
 ) -> tuple[torch.Tensor]:
-    gate_grad = torch.empty_like(gate)
-    up_grad = torch.empty_like(up)
+    x_size = x.size()
+    stride = divide_if_divisible(x_size[-1], 2)
+    x_grad = torch.empty_like(x)
 
     if kernel_backend == KernelBackend.triton:
-        num_elements = gate.numel()
+        num_blocks_per_stride = ceil_divide(stride, BLOCK_SIZE)
+        num_strides = divide_if_divisible(x.numel(), x.size(-1))
 
-        with torch.device(gate.device):
-            swiglu_unchunked_backward_triton_kernel[ceil_divide(num_elements, BLOCK_SIZE),](
-                gate_ptr=gate,
-                up_ptr=up,
+        with torch.device(x.device):
+            swiglu_unchunked_backward_triton_kernel[(num_strides * num_blocks_per_stride,)](
+                x_ptr=x,
                 output_grad_ptr=output_grad,
-                gate_grad_ptr=gate_grad,
-                up_grad_ptr=up_grad,
+                x_grad_ptr=x_grad,
                 num_elements=num_elements,
                 BLOCK_SIZE=BLOCK_SIZE,
             )
