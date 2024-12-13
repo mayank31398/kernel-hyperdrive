d5767e5e4ab55f2cd3274c3f8074d9f650a3ac51
Mayank Mishra <mayank31398@gmail.com>
2024-12-13 14:07:14 -0500

    reset cache
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- tests/kernels/contiguous_count_test.py
+++ tests/kernels/contiguous_count_test.py
@@ -31,9 +31,9 @@ class ContiguousCountTest(TestCommons):
     ) -> None:
         x = torch.randint(0, MAX_EXPERTS, (size,), device=device, dtype=torch.long)
 
-        torch.cuda.set_device(x.device)
-        with torch.cuda._DeviceGuard(x.device):
+        with torch.device(x.device):
             z_kernel = function(x=x, size=MAX_EXPERTS, kernel_backend=kernel_backend, BLOCK_SIZE_B=BLOCK_SIZE_B)
+
         z_expected = x.view(-1).bincount(minlength=MAX_EXPERTS)
 
         self.assert_equal_tensors(z_kernel, z_expected, True)
