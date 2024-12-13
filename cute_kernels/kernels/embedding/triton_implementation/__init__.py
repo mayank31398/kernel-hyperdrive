92e2632715fb328139ff4ffd789d1706793baeb6
Mayank Mishra <mayank31398@gmail.com>
2024-12-13 14:59:03 -0500

    kernel
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/embedding/triton_implementation/__init__.py
+++ cute_kernels/kernels/embedding/triton_implementation/__init__.py
@@ -1,2 +1,33 @@
-from .kernels_backward import embedding_backward_triton_kernel
+import torch
+
+from ....constants import LIBRARY_NAME
+from ....utils import ceil_divide, cute_op
 from .kernels_forward import embedding_forward_triton_kernel
+
+
+_KERNEL_NAME = "embedding_forward_triton"
+
+
+@cute_op(f"{LIBRARY_NAME}::{_KERNEL_NAME}", mutates_args={"output"})
+def embedding_forward_triton(
+    input_ids: torch.Tensor,
+    weight: torch.Tensor,
+    output: torch.Tensor,
+    BLOCK_SIZE_B: int,
+    BLOCK_SIZE_H: int,
+) -> None:
+    num_elements = input_ids.numel()
+    hidden_size = weight.size(-1)
+
+    with torch.device(input_ids.device):
+        embedding_forward_triton_kernel[
+            (ceil_divide(num_elements, BLOCK_SIZE_B), ceil_divide(hidden_size, BLOCK_SIZE_H))
+        ](
+            x_ptr=input_ids,
+            weight_ptr=weight,
+            output_ptr=output,
+            B=num_elements,
+            H=hidden_size,
+            BLOCK_SIZE_B=BLOCK_SIZE_B,
+            BLOCK_SIZE_H=BLOCK_SIZE_H,
+        )
