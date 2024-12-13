dac0f5a052de1144cd6446593c42cf358c50fe9f
Mayank Mishra <mayank31398@gmail.com>
2024-12-13 16:30:36 -0500

    npo2
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/utils/math.py
+++ cute_kernels/utils/math.py
@@ -22,3 +22,15 @@ def get_powers_of_2(start: int, end: int) -> list[int]:
 def divide_if_divisible(dividend: int, divisor: int, msg: str = "") -> int:
     assert dividend % divisor == 0, msg
     return dividend // divisor
+
+
+def next_power_of_2(n: int):
+    n -= 1
+    n = n | (n >> 1)
+    n = n | (n >> 2)
+    n = n | (n >> 4)
+    n = n | (n >> 8)
+    n = n | (n >> 16)
+    n = n | (n >> 32)
+    n += 1
+    return n
