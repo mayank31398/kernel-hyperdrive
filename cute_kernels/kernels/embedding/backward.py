97e741ee7db0cc8771f081fe726b728cd2782eb6
Mayank Mishra <mayank31398@gmail.com>
2024-12-13 15:04:01 -0500

    kernel
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/embedding/backward.py
+++ cute_kernels/kernels/embedding/backward.py
@@ -3,7 +3,7 @@ import torch
 from ...constants import MAX_TRITON_BLOCK_SIZE
 from ...enums import KernelBackend
 from ...utils import CutoTuneConfig, ceil_divide, cutotune, get_cartesian_product_cutotune_configs, get_powers_of_2
-from .triton_implementation import embedding_backward_triton_kernel
+from .triton_implementation import embedding_backward_triton
 
 
 @cutotune(
@@ -24,27 +24,19 @@ def _backward(
     BLOCK_SIZE_B: int,
     BLOCK_SIZE_H: int,
 ) -> torch.Tensor:
-    hidden_size = weight.size(-1)
-    num_elements = input_ids.numel()
-
     weight_grad = torch.zeros_like(weight)
 
     if kernel_backend == KernelBackend.triton:
         if weight.dtype == torch.bfloat16:
             raise NotImplementedError("bf16 is not supported with triton backend for backward for embeddings")
 
-        with torch.device(input_ids.device):
-            embedding_backward_triton_kernel[
-                (ceil_divide(num_elements, BLOCK_SIZE_B), ceil_divide(hidden_size, BLOCK_SIZE_H))
-            ](
-                x_ptr=input_ids,
-                output_grad_ptr=output_grad,
-                weight_grad_ptr=weight_grad,
-                B=num_elements,
-                H=hidden_size,
-                BLOCK_SIZE_B=BLOCK_SIZE_B,
-                BLOCK_SIZE_H=BLOCK_SIZE_H,
-            )
+        embedding_backward_triton(
+            input_ids=input_ids,
+            output_grad=output_grad,
+            weight_grad=weight_grad,
+            BLOCK_SIZE_B=BLOCK_SIZE_B,
+            BLOCK_SIZE_H=BLOCK_SIZE_H,
+        )
     else:
         raise ValueError(f"unexpected kernel_backend ({kernel_backend})")
 
