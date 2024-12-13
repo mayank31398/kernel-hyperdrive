e31a03740c0ddbbe01ab96c63984acbff5c176db
Mayank Mishra <mayank31398@gmail.com>
2024-12-13 14:19:09 -0500

    kernel
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/add/add_scalar/cuda_implementation/__init__.py
+++ cute_kernels/kernels/add/add_scalar/cuda_implementation/__init__.py
@@ -9,7 +9,7 @@ _KERNEL_NAME = "add_scalar_forward_cuda"
 
 
 @cute_op(f"{LIBRARY_NAME}::{_KERNEL_NAME}", mutates_args={"output"})
-def add_scalar_forward_cuda_kernel(
+def add_scalar_forward_cuda(
     x: torch.Tensor, y: float, output: torch.Tensor, vector_instruction_width: int, BLOCK_SIZE: int
 ) -> None:
     KernelRegistry.get_kernel(_KERNEL_NAME)(x, y, output, vector_instruction_width, BLOCK_SIZE)
