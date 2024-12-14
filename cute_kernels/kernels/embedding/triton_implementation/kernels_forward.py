71a2cc6dcab16dd8075db2bc94d8c0b5e9deb0d4
Mayank Mishra <mayank31398@gmail.com>
2024-12-13 22:56:43 -0500

    cleanup
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/embedding/triton_implementation/kernels_forward.py
+++ cute_kernels/kernels/embedding/triton_implementation/kernels_forward.py
@@ -1,5 +1,14 @@
+import torch
 import triton
 import triton.language as tl
+
+from ....constants import LIBRARY_NAME
+from ....utils import ceil_divide, cute_op
+from .kernels_backward import embedding_backward_triton_kernel
+from .kernels_forward import embedding_forward_triton_kernel
+
+
+_KERNEL_NAME = "embedding_forward_triton"
 
 
 @triton.jit
@@ -29,3 +38,28 @@ def embedding_forward_triton_kernel(
 
     output_ptrs = output_ptr + indices_b[:, None] * H + indices_h[None, :]
     tl.store(output_ptrs, word_embeddings, mask=mask_b[:, None] & mask_h[None, :])
+
+
+@cute_op(f"{LIBRARY_NAME}::{_KERNEL_NAME}", mutates_args={"output"})
+def embedding_forward_triton(
+    input_ids: torch.Tensor,
+    weight: torch.Tensor,
+    output: torch.Tensor,
+    BLOCK_SIZE_B: int,
+    BLOCK_SIZE_H: int,
+) -> None:
+    num_elements = input_ids.numel()
+    hidden_size = weight.size(-1)
+
+    with torch.device(input_ids.device):
+        embedding_forward_triton_kernel[
+            (ceil_divide(num_elements, BLOCK_SIZE_B), ceil_divide(hidden_size, BLOCK_SIZE_H))
+        ](
+            x_ptr=input_ids,
+            weight_ptr=weight,
+            output_ptr=output,
+            B=num_elements,
+            H=hidden_size,
+            BLOCK_SIZE_B=BLOCK_SIZE_B,
+            BLOCK_SIZE_H=BLOCK_SIZE_H,
+        )
