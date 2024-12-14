2d26b68fa3d2b9e9e3c40509efa9bb52aa567361
Mayank Mishra <mayank31398@gmail.com>
2024-12-13 22:23:01 -0500

    swiglu_unchunked
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/swiglu_unchunked/backward.py
+++ cute_kernels/kernels/swiglu_unchunked/backward.py
@@ -1,8 +1,8 @@
 import torch
 
 from ...enums import KernelBackend
-from ...utils import CutoTuneConfig, ceil_divide, cutotune, get_cartesian_product_cutotune_configs, get_powers_of_2
-from .triton_implementation import swiglu_unchunked_backward_triton_kernel
+from ...utils import CutoTuneConfig, cutotune, get_cartesian_product_cutotune_configs, get_powers_of_2
+from .triton_implementation import swiglu_backward_triton
 
 
 @cutotune(
@@ -27,16 +27,9 @@ def _backward(
     x_grad = torch.empty_like(x)
 
     if kernel_backend == KernelBackend.triton:
-        with torch.device(x.device):
-            swiglu_unchunked_backward_triton_kernel[(ceil_divide(B, BLOCK_SIZE_B), ceil_divide(H, BLOCK_SIZE_H))](
-                x_ptr=x,
-                output_grad_ptr=output_grad,
-                x_grad_ptr=x_grad,
-                B=B,
-                H=H,
-                BLOCK_SIZE_B=BLOCK_SIZE_B,
-                BLOCK_SIZE_H=BLOCK_SIZE_H,
-            )
+        swiglu_backward_triton(
+            x=x, output_grad=output_grad, x_grad=x_grad, BLOCK_SIZE_B=BLOCK_SIZE_B, BLOCK_SIZE_H=BLOCK_SIZE_H
+        )
     else:
         raise ValueError(f"unexpected kernel_backend ({kernel_backend})")
 
--- cute_kernels/kernels/swiglu_unchunked/triton_implementation/__init__.py
+++ cute_kernels/kernels/swiglu_unchunked/triton_implementation/__init__.py
@@ -26,24 +26,24 @@ def swiglu_forward_triton(x: torch.Tensor, output: torch.Tensor, BLOCK_SIZE_B: i
         )
 
 
-@cute_op(f"{LIBRARY_NAME}::{_BACKWARD_KERNEL_NAME}", mutates_args={"gate_grad", "up_grad"})
+@cute_op(f"{LIBRARY_NAME}::{_BACKWARD_KERNEL_NAME}", mutates_args={"x_grad"})
 def swiglu_backward_triton(
-    gate: torch.Tensor,
-    up: torch.Tensor,
+    x: torch.Tensor,
     output_grad: torch.Tensor,
-    gate_grad: torch.Tensor,
-    up_grad: torch.Tensor,
-    BLOCK_SIZE: int,
+    x_grad: torch.Tensor,
+    BLOCK_SIZE_B: int,
+    BLOCK_SIZE_H: int,
 ) -> None:
-    num_elements = gate.numel()
+    H = x.size(-1)
+    B = x.numel() // H
 
-    with torch.device(gate.device):
-        swiglu_backward_triton_kernel[(ceil_divide(num_elements, BLOCK_SIZE),)](
-            gate_ptr=gate,
-            up_ptr=up,
+    with torch.device(x.device):
+        swiglu_unchunked_backward_triton_kernel[(ceil_divide(B, BLOCK_SIZE),)](
+            x_ptr=x,
             output_grad_ptr=output_grad,
-            gate_grad_ptr=gate_grad,
-            up_grad_ptr=up_grad,
-            num_elements=num_elements,
-            BLOCK_SIZE=BLOCK_SIZE,
+            x_grad_ptr=x_grad,
+            B=B,
+            H=H,
+            BLOCK_SIZE_B=BLOCK_SIZE_B,
+            BLOCK_SIZE_H=BLOCK_SIZE_H,
         )
