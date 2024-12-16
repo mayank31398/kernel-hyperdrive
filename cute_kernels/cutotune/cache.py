54f72a1c08e65b5873ea0ab9d3eac25d9e3f22bc
Mayank Mishra <mayank31398@gmail.com>
2024-12-16 13:44:37 -0500

    fully serialized cache
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/cutotune/cache.py
+++ cute_kernels/cutotune/cache.py
@@ -46,7 +46,7 @@ class _CutoTuneCache:
         for function_hash in all_configs:
             best_configs[function_hash] = {}
             for lookup_key in all_configs[function_hash]:
-                config_time = min(self.full_cache[function_hash][lookup_key], key=lambda x: x[1])
+                config_time = min(all_configs[function_hash][lookup_key], key=lambda x: x[1])
                 best_configs[function_hash][lookup_key] = {"config": config_time[0], "time": config_time[1]}
 
         full_cache_serialized = {"all_configs": all_configs, "best_configs": best_configs}
