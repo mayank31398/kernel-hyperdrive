33e3329b7961892582f5786b4ced6793071c5a2c
Mayank Mishra <mayank31398@gmail.com>
2024-12-15 23:52:44 -0500

    cache
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/cutotune/cache.py
+++ cute_kernels/cutotune/cache.py
@@ -5,6 +5,9 @@ from collections import defaultdict
 from .config import CutoTuneConfig
 
 
+_CUTOTUNE_CACHE_FILENAME = "cute.json"
+
+
 class _CutoTuneCache:
     def __init__(self, filename: str) -> None:
         self.full_cache = defaultdict(lambda: defaultdict(list))
@@ -21,12 +24,24 @@ class _CutoTuneCache:
     def add_config(self, function_hash: str, lookup_key: str, config: CutoTuneConfig, time: float) -> None:
         self.full_cache[function_hash][lookup_key].append((config, time))
 
+    def save(self) -> None:
+        json.dump(self.full_cache, open(_CUTOTUNE_CACHE_FILENAME, "w"), indent=4)
+
 
 _CUTOTUNE_CACHE = None
 
 
 def get_cutotune_cache() -> _CutoTuneCache:
-    global _CUTOTUNE_CACHE
+    global _CUTOTUNE_CACHE, _CUTOTUNE_CACHE_FILENAME
+
     if _CUTOTUNE_CACHE is None:
-        _CUTOTUNE_CACHE = _CutoTuneCache("cute.json")
+        _CUTOTUNE_CACHE = _CutoTuneCache(_CUTOTUNE_CACHE_FILENAME)
+
     return _CUTOTUNE_CACHE
+
+
+def save_cutotune_cache() -> None:
+    global _CUTOTUNE_CACHE
+    assert _CUTOTUNE_CACHE is not None
+
+    _CUTOTUNE_CACHE.save()
