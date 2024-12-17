0b396f66295a248b093db68c950a6b8caccb08ba
Mayank Mishra <mayank31398@gmail.com>
2024-12-17 15:05:04 -0500

    ->
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- tools/build_cutotune_cache.py
+++ tools/build_cutotune_cache.py
@@ -22,9 +22,7 @@ from cute_kernels import (
 def get_tensor_metadata() -> list[tuple[torch.dtype, tuple[int]]]:
     metadata_list = []
     for dtype in get_dtypes():
-        for size in get_1d_tensor_sizes():
-            metadata_list.append((size, dtype))
-        for size in get_2d_tensor_sizes():
+        for size in [104857600, (1024, 102400)]:
             metadata_list.append((size, dtype))
 
     return metadata_list
@@ -35,49 +33,49 @@ def forward_backward(kernel: Callable, *args, **kwargs) -> None:
     output.sum().backward()
 
 
-for metadata in tqdm(get_tensor_metadata()):
-    size, dtype = metadata
-    x = torch.randn(size, dtype=dtype, device=torch.cuda.current_device(), requires_grad=True)
-
-    forward_backward(add_scalar_cute, x, 3)
-    forward_backward(add_tensor_cute, x, x)
-    forward_backward(swiglu_cute, x, x)
-
-    forward_backward(rmsnorm_cute, x, weight=None, eps=1e-5)
-    forward_backward(
-        rmsnorm_cute,
-        x,
-        weight=torch.randn(x.size(-1), dtype=dtype, device=torch.cuda.current_device(), requires_grad=True),
-        eps=1e-5,
-    )
-
-
 for dtype in get_dtypes():
-    for size in get_2d_tensor_sizes():
+    for size in [104857600]:
+        x = torch.randn(size, dtype=dtype, device=torch.cuda.current_device(), requires_grad=True)
+
+        forward_backward(add_scalar_cute, x, 3)
+        forward_backward(add_tensor_cute, x, x)
+        forward_backward(swiglu_cute, x, x)
+
+        forward_backward(rmsnorm_cute, x, weight=None, eps=1e-5)
         forward_backward(
-            swiglu_unchunked_cute,
-            torch.randn(
-                (size[0], ceil_divide(size[1], 2) * 2),
-                dtype=metadata[1],
-                device=torch.cuda.current_device(),
-                requires_grad=True,
-            ),
+            rmsnorm_cute,
+            x,
+            weight=torch.randn(x.size(-1), dtype=dtype, device=torch.cuda.current_device(), requires_grad=True),
+            eps=1e-5,
         )
 
-for size in tqdm(get_1d_tensor_sizes()):
-    x = torch.randint(0, 64, (size,), device=torch.cuda.current_device(), dtype=torch.long)
-    contiguous_count_cute(x, 64)
 
-for dtype in [torch.float32, torch.float16]:
-    for input_ids_size in [(51, 17), (19, 239), (7, 7537), (9, 1749)]:
-        for weight_size in [(7153, 937), (27153, 1937), (97153, 2937), (17153, 31937)]:
-            forward_backward(
-                embedding_cute,
-                input_ids=torch.randint(
-                    0, weight_size[0] - 1, input_ids_size, device=torch.cuda.current_device(), dtype=torch.long
-                ),
-                weight=torch.randn(weight_size, device=torch.cuda.current_device(), dtype=dtype, requires_grad=True),
-            )
+# for dtype in get_dtypes():
+#     for size in get_2d_tensor_sizes():
+#         forward_backward(
+#             swiglu_unchunked_cute,
+#             torch.randn(
+#                 (size[0], ceil_divide(size[1], 2) * 2),
+#                 dtype=metadata[1],
+#                 device=torch.cuda.current_device(),
+#                 requires_grad=True,
+#             ),
+#         )
+
+# for size in tqdm(get_1d_tensor_sizes()):
+#     x = torch.randint(0, 64, (size,), device=torch.cuda.current_device(), dtype=torch.long)
+#     contiguous_count_cute(x, 64)
+
+# for dtype in [torch.float32, torch.float16]:
+#     for input_ids_size in [(51, 17), (19, 239), (7, 7537), (9, 1749)]:
+#         for weight_size in [(7153, 937), (27153, 1937), (97153, 2937), (17153, 31937)]:
+#             forward_backward(
+#                 embedding_cute,
+#                 input_ids=torch.randint(
+#                     0, weight_size[0] - 1, input_ids_size, device=torch.cuda.current_device(), dtype=torch.long
+#                 ),
+#                 weight=torch.randn(weight_size, device=torch.cuda.current_device(), dtype=dtype, requires_grad=True),
+#             )
 
 
-save_cutotune_cache()
+# save_cutotune_cache()
