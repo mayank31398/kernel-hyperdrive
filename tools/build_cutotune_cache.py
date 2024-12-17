9f32bf21f3a01ae147b6604dfffdae3730e35a87
Mayank Mishra <mayank31398@gmail.com>
2024-12-17 14:16:40 -0500

    ->
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- tools/build_cutotune_cache.py
+++ tools/build_cutotune_cache.py
@@ -23,7 +23,7 @@ def get_tensor_metadata() -> list[tuple[torch.dtype, tuple[int]]]:
     metadata_list = []
     for dtype in get_dtypes():
         for size in get_1d_tensor_sizes():
-            metadata_list.append((size, dtype))
+            metadata_list.append(((size,), dtype))
         for size in get_2d_tensor_sizes():
             metadata_list.append((size, dtype))
 
