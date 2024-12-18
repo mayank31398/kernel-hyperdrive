9aac0a7c52ef7a573e74de29ab26fa1f84e61c5e
Mayank Mishra <mayank31398@gmail.com>
2024-12-18 01:46:19 -0500

    rename
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/cute_inductor/rmsnorm.py
+++ cute_kernels/cute_inductor/rmsnorm.py
@@ -8,20 +8,19 @@ from .constants import CALL_FUNCTION
 
 def replace_rmsnorm(gm: GraphModule, node: Node) -> None:
     if node.op == CALL_FUNCTION and node.target == torch.rms_norm:
+        # delete normalized_shape from the args (position 1)
+        args = node.args[:1] + node.args[2:]
+        kwargs = {key: value for key, value in node.kwargs.items()}
+
+        # delete normalized_shape from the kwargs
+        kwargs.pop("normalized_shape", None)
+
+        # rename input to x
+        input = kwargs.pop("input", None)
+        if input is not None:
+            kwargs["x"] = input
+
         with gm.graph.inserting_after(node):
-            # delete normalized_shape from the args (position 1)
-            args = node.args[:1] + node.args[2:]
-
-            kwargs = {key: value for key, value in node.kwargs.items()}
-
-            # delete normalized_shape from the kwargs
-            kwargs.pop("normalized_shape", None)
-
-            # rename input to x
-            input = kwargs.pop("input", None)
-            if input is not None:
-                kwargs["x"] = input
-
             new_node = gm.graph.call_function(rmsnorm_cute, args=tuple(args), kwargs=kwargs)
 
         node.replace_all_uses_with(new_node)
