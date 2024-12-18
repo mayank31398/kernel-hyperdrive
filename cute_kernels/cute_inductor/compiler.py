3f40ab90a4fd744c7e7c10693f65f40498577dca
Mayank Mishra <mayank31398@gmail.com>
2024-12-18 11:44:55 -0500

    change iteration order
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/cute_inductor/compiler.py
+++ cute_kernels/cute_inductor/compiler.py
@@ -14,9 +14,11 @@ _DEBUG_CUTEINDUCTOR = get_boolean_env_variable("DEBUG_CUTEINDUCTOR", True)
 
 class CuteInductor:
     def __init__(
-        self, use_inductor: bool = True, replace_functions=[replace_rmsnorm, replace_swiglu_unchunked, replace_swiglu]
+        self,
+        use_torch_inductor_after_cute_inductor: bool = True,
+        replace_functions=[replace_rmsnorm, replace_swiglu_unchunked, replace_swiglu],
     ) -> None:
-        self.use_inductor = use_inductor
+        self.use_torch_inductor_after_cute_inductor = use_torch_inductor_after_cute_inductor
         self.replace_functions = replace_functions
 
     def compiler(self, gm: torch.fx.GraphModule, example_inputs: list[torch.Tensor]) -> Callable:
@@ -34,7 +36,7 @@ class CuteInductor:
             print("graph after cute inductor")
             gm.print_readable()
 
-        if self.use_inductor:
+        if self.use_torch_inductor_after_cute_inductor:
             inductor = lookup_backend("inductor")
             compiled = inductor(gm, example_inputs)
         else:
