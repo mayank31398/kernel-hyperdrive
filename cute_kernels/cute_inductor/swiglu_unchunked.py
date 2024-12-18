fd415a153e3bfb0ce46af59d1ba0af8706ed76ad
Mayank Mishra <mayank31398@gmail.com>
2024-12-18 02:20:03 -0500

    rename
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/cute_inductor/swiglu_unchunked.py
+++ cute_kernels/cute_inductor/swiglu_unchunked.py
@@ -11,18 +11,27 @@ def replace_swiglu_unchunked(gm: GraphModule, node: Node) -> None:
     if not (node.op == CALL_METHOD and node.target == torch.chunk.__name__):
         return
 
+    x_dim = node.kwargs.get("input", node.args[0]).dim()
+
     chunks = node.kwargs.get("chunks", node.args[1])
     if chunks != 2:
         return
 
-        # if len(node.args) == 2 and node.args[1] == 2:
-        #     with gm.graph.inserting_after(node):
-        #         # Create a new node for the custom chunk_silu function
-        #         new_node = gm.graph.call_function(chunk_silu, args=(node.args[0],))
+    dim = node.kwargs.get("dim", node.args[2])
+    # dim should be last dim or skip
+    if dim not in [-1, x_dim - 1]:
+        return
 
-        #     # Replace all uses of the old node with the new node
-        #     node.replace_all_uses_with(new_node)
-        #     gm.graph.erase_node(node)
+    print(node)
 
-        # node.replace_all_uses_with(new_node)
-        # gm.graph.erase_node(node)
+    # if len(node.args) == 2 and node.args[1] == 2:
+    #     with gm.graph.inserting_after(node):
+    #         # Create a new node for the custom chunk_silu function
+    #         new_node = gm.graph.call_function(chunk_silu, args=(node.args[0],))
+
+    #     # Replace all uses of the old node with the new node
+    #     node.replace_all_uses_with(new_node)
+    #     gm.graph.erase_node(node)
+
+    # node.replace_all_uses_with(new_node)
+    # gm.graph.erase_node(node)
