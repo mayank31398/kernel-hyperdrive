eccc64da5a8dadcae2b26d0200c9bcb9dfd1d5a3
Mayank Mishra <mayank31398@gmail.com>
2024-12-18 00:58:00 -0500

    rename
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/cute_inductor/rmsnorm.py
+++ cute_kernels/cute_inductor/rmsnorm.py
@@ -8,11 +8,9 @@ from .constants import CALL_FUNCTION
 
 
 def replace_rmsnorm(gm: GraphModule, node: Node) -> None:
-    if not (node.op == CALL_FUNCTION and node.target in [torch.rms_norm, F.rms_norm]):
-        return
+    if node.op == CALL_FUNCTION and node.target in [torch.rms_norm, F.rms_norm]:
+        with gm.graph.inserting_after(node):
+            new_node = gm.graph.call_function(rmsnorm_cute, args=(node.args[0], node.args[2], node.args[3]))
 
-    with gm.graph.inserting_after(node):
-        new_node = gm.graph.call_function(rmsnorm_cute, args=(node.args[0], node.args[2], node.args[3]))
-
-    node.replace_all_uses_with(new_node)
-    gm.graph.erase_node(node)
+        node.replace_all_uses_with(new_node)
+        gm.graph.erase_node(node)
