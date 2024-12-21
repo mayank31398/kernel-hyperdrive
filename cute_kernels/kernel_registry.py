bb6b0ce610942afe590384756090f128ca14fdd1
Mayank Mishra <mayank31398@gmail.com>
2024-12-20 22:00:10 -0500

    jit
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernel_registry.py
+++ cute_kernels/kernel_registry.py
@@ -6,20 +6,24 @@ import yaml
 from torch.utils.cpp_extension import load as load_cpp_extension
 
 
-class KernelRegistry:
+class _CUDA_JIT:
     module_name = "cute_cuda_kernels"
     build_directory = "build"
     cuda_kernel_registry = {}
     kernel_registry_yaml = yaml.safe_load(open(os.path.join(os.path.dirname(__file__), "kernel_registry.yml"), "r"))
 
+    def __init__(self, kernel_name: str) -> None:
+        self.kernel_name = kernel_name
+        self._kernel = None
+
     @staticmethod
     def get_kernel(name: str) -> Callable:
-        kernel = KernelRegistry.cuda_kernel_registry.get(name, None)
+        kernel = _CUDA_JIT.cuda_kernel_registry.get(name, None)
 
         # if kernel is compiled, we return the torch op since its compatible with torch compile
         if kernel is None:
-            KernelRegistry.compile_kernel(name)
-            kernel = KernelRegistry.get_kernel(name)
+            _CUDA_JIT.compile_kernel(name)
+            kernel = _CUDA_JIT.get_kernel(name)
 
         return kernel
 
@@ -29,7 +33,7 @@ class KernelRegistry:
         function_map = []
         all_functions = []
         source_map = []
-        for module in KernelRegistry.kernel_registry_yaml:
+        for module in _CUDA_JIT.kernel_registry_yaml:
             function_map.append(module["functions"])
             all_functions.extend(module["functions"])
 
@@ -39,7 +43,7 @@ class KernelRegistry:
 
         assert len(all_functions) == len(set(all_functions)), "function names are not unique"
 
-        build_directory = KernelRegistry.build_directory
+        build_directory = _CUDA_JIT.build_directory
         os.makedirs(build_directory, exist_ok=True)
 
         # find which files the function belongs to
@@ -48,7 +52,7 @@ class KernelRegistry:
                 break
 
         module = load_cpp_extension(
-            f"{KernelRegistry.module_name}_{index}",
+            f"{_CUDA_JIT.module_name}_{index}",
             sources=source_map[index],
             with_cuda=True,
             extra_cflags=["-O3", "-Wall", "-shared", "-fPIC", "-fdiagnostics-color"],
@@ -58,4 +62,20 @@ class KernelRegistry:
 
         # populate all functions from the file
         for function in function_map[index]:
-            KernelRegistry.cuda_kernel_registry[function] = getattr(module, function)
+            _CUDA_JIT.cuda_kernel_registry[function] = getattr(module, function)
+
+    def __call__(self, *args, **kwargs):
+        if self._kernel is None:
+            self._kernel = _CUDA_JIT.compile_kernel(self.kernel_name)
+
+        return self._kernel(*args, **kwargs)
+
+
+KernelRegistry = _CUDA_JIT
+
+
+def cuda_jit(kernel_name: str) -> Callable:
+    def inner(function: Callable) -> Callable:
+        return _CUDA_JIT(kernel_name).__call__
+
+    return inner
--- cute_kernels/kernels/add/add_scalar/cuda_implementation/kernels_forward.py
+++ cute_kernels/kernels/add/add_scalar/cuda_implementation/kernels_forward.py
@@ -1,7 +1,7 @@
 import torch
 
 from .....constants import LIBRARY_NAME
-from .....kernel_registry import KernelRegistry
+from .....kernel_registry import KernelRegistry, cuda_jit
 from .....utils import cute_op
 
 
@@ -9,7 +9,7 @@ _KERNEL_NAME = "add_scalar_forward_cuda"
 
 
 @cute_op(f"{LIBRARY_NAME}::{_KERNEL_NAME}", mutates_args={"output"})
+@cuda_jit(_KERNEL_NAME)
 def add_scalar_forward_cuda(
     x: torch.Tensor, y: float, output: torch.Tensor, vector_instruction_width: int, BLOCK_SIZE: int
-) -> None:
-    KernelRegistry.get_kernel(_KERNEL_NAME)(x, y, output, vector_instruction_width, BLOCK_SIZE)
+) -> None: ...
