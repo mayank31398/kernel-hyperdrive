ce9a82fd4e7db7644e3ac1b6ca2fc9e318229857
Mayank Mishra <mayank31398@gmail.com>
2024-12-22 00:26:52 -0500

    drop duplicate tests
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/include/activations.h
+++ cute_kernels/include/activations.h
@@ -3,15 +3,8 @@
 #include "dtypes/all.h"
 
 template <typename input_T, typename output_T>
-__device__ output_T sigmoid(const input_T &x) {
+inline __device__ output_T sigmoid(const input_T &x) {
     fp32 x_fp32 = DType<input_T>::upcast(x);
-
-    if (x >= 0) {
-        x_fp32 = 1 / (1 + expf(-x_fp32));
-    } else {
-        x_fp32 = expf(x_fp32);
-        x_fp32 = x_fp32 / (1 + x_fp32);
-    }
-
+    x_fp32 = 1 / (1 + expf(-x_fp32));
     return DType<output_T>::downcast(x_fp32);
 }
