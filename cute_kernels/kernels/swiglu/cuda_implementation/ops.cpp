a234cf340647b2e632b36108776711b049b31b2d
Mayank Mishra <mayank31398@gmail.com>
2024-12-23 00:10:44 -0500

    fix
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/swiglu/cuda_implementation/ops.cpp
+++ cute_kernels/kernels/swiglu/cuda_implementation/ops.cpp
@@ -3,7 +3,7 @@
 void swiglu_forward_cuda(const torch::Tensor &gate,
                          const torch::Tensor &up,
                          torch::Tensor &output,
-                         const int &vector_instruction_width,
+                         int &vector_instruction_width,
                          const int &BLOCK_SIZE);
 
 void swiglu_backward_cuda(const torch::Tensor &gate,
