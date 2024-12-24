c058bb67633390b32f39ac30f859dd6fa83b3f79
Mayank Mishra <mayank31398@gmail.com>
2024-12-24 18:50:41 -0500

    cuda
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/contiguous_count/cuda_implementation/kernels_forward.cu
+++ cute_kernels/kernels/contiguous_count/cuda_implementation/kernels_forward.cu
@@ -8,8 +8,16 @@
 
 #define MAX_ALLOWED_C 16384
 
-inline __device__ void _initialize_shared_memory_and_output(
-    uint32 *output_shared, int32 *output, const int &C, const int &thread_id, const int &num_loops_C) {
+__global__ void _contiguous_count_cuda_kernel(const int32 *x,
+                                              int32 *output,
+                                              const uint64 num_elements,
+                                              const uint32 C) {
+    const uint64 thread_id = get_global_thread_id();
+    const int num_loops_C = (C + blockDim.x - 1) / blockDim.x;
+
+    extern __shared__ uint32 output_shared[];
+
+    // initialize shared memory and output
     // clang-format off
     #pragma unroll
     // clang-format on
@@ -20,46 +28,34 @@ inline __device__ void _initialize_shared_memory_and_output(
             output[index] = 0;
         }
     }
-}
 
-template <int vector_instruction_width>
-__global__ void _contiguous_count_cuda_kernel(const int32 *x,
-                                              int32 *output,
-                                              const uint64 num_elements,
-                                              const uint32 C) {
-    const uint64 thread_id = get_global_thread_id();
-    const int num_loops_C = (C + blockDim.x - 1) / blockDim.x;
-
-    __shared__ uint32 output_shared[MAX_ALLOWED_C];
-    _initialize_shared_memory_and_output(output_shared, output, C, thread_id, num_loops_C);
     __syncthreads();
 
-    const int num_elements_per_SM = (num_elements + gridDim.x - 1) / gridDim.x;
-    const int num_elements_per_loop = blockDim.x * vector_instruction_width;
-    const int num_loops_B = (num_elements_per_SM + num_elements_per_loop - 1) / num_elements_per_loop;
+    // count the number of occurances of each number in x
+    const int num_elements_per_block = (num_elements + gridDim.x - 1) / gridDim.x;
+    const int num_loops_B = (num_elements_per_block + blockDim.x - 1) / blockDim.x;
 
-    const int start = blockIdx.x * num_elements_per_SM;
+    const int start = blockIdx.x * num_elements_per_block;
 
     for (int i = 0; i < num_loops_B; i++) {
-        int index = thread_id + i * num_loops_B + start;
-        for (int j = 0; j < vector_instruction_width; j++) {
-            // x_vec[index + j]
+        const int index = start + i * num_loops_B + thread_id;
+        if (index < num_elements) {
+            atomicAdd(&output_shared[x[index]], 1);
         }
     }
 
-    // // TODO add code here
+    __syncthreads();
 
-    // __syncthreads();
-
-    // // clang-format off
-    // #pragma unroll
-    // // clang-format on
-    // for (int i = 0; i < num_loops; i++) {
-    //     const int index = thread_id + i * blockDim.x;
-    //     if (index < C) {
-    //         atomicAdd(&output[index], output_shared[index]);
-    //     }
-    // }
+    // write the output to the global memory
+    // clang-format off
+    #pragma unroll
+    // clang-format on
+    for (int i = 0; i < num_loops_C; i++) {
+        const int index = thread_id + i * blockDim.x;
+        if (index < C) {
+            atomicAdd(&output[index], output_shared[index]);
+        }
+    }
 }
 
 void contiguous_count_cuda(const torch::Tensor &x, const torch::Tensor &output, const int &C, const int &BLOCK_SIZE) {
@@ -72,6 +68,6 @@ void contiguous_count_cuda(const torch::Tensor &x, const torch::Tensor &output, 
     const int num_elements_per_block = BLOCK_SIZE << 2;
     const int NUM_BLOCKS = (num_elements + num_elements_per_block - 1) / num_elements_per_block;
 
-    _contiguous_count_cuda_kernel<4>
-        <<<NUM_BLOCKS, BLOCK_SIZE>>>(x.data_ptr<int32>(), output.data_ptr<int32>(), num_elements, C);
+    _contiguous_count_cuda_kernel<<<NUM_BLOCKS, BLOCK_SIZE, C * sizeof(int32)>>>(
+        x.data_ptr<int32>(), output.data_ptr<int32>(), num_elements, C);
 }
