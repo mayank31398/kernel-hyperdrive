8db612bf2ee807d2ee241cc11d0351fdf0ad4d40
Mayank Mishra <mayank31398@gmail.com>
2024-12-24 16:58:27 -0500

    cuda
    Signed-off-by: Mayank Mishra <mayank31398@gmail.com>

--- cute_kernels/kernels/contiguous_count/cuda_implementation/kernels_forward.cu
+++ cute_kernels/kernels/contiguous_count/cuda_implementation/kernels_forward.cu
@@ -8,28 +8,44 @@
 
 #define MAX_ALLOWED_C 16384
 
+inline __device__ void _initialize_shared_memory_and_output(
+    uint32 *output_shared, int32 *output, const int &C, const int &thread_id, const int &num_loops_C) {
+    // clang-format off
+    #pragma unroll
+    // clang-format on
+    for (int i = 0; i < num_loops_C; i++) {
+        const int index = thread_id + i * blockDim.x;
+        if (index < C) {
+            output_shared[index] = 0;
+            output[index] = 0;
+        }
+    }
+}
+
 template <int vector_instruction_width>
 __global__ void _contiguous_count_cuda_kernel(const int32 *x,
                                               int32 *output,
                                               const uint64 num_elements,
                                               const uint32 C) {
     const uint64 thread_id = get_global_thread_id();
-    const int num_loops = (C + blockDim.x - 1) / blockDim.x;
+    const int num_loops_C = (C + blockDim.x - 1) / blockDim.x;
 
     __shared__ uint32 output_shared[MAX_ALLOWED_C];
+    _initialize_shared_memory_and_output(output_shared, output, C, thread_id, num_loops_C);
+    __syncthreads();
 
-    // clang-format off
-    #pragma unroll
-    // clang-format on
-    for (int i = 0; i < num_loops; i++) {
-        const int index = thread_id + i * blockDim.x;
-        if (index < C) {
-            output_shared[index] = 0;
-            output[index] = 3;
+    const int num_elements_per_SM = (num_elements + gridDim.x - 1) / gridDim.x;
+    const int num_elements_per_loop = blockDim.x * vector_instruction_width;
+    const int num_loops_B = (num_elements_per_SM + num_elements_per_loop - 1) / num_elements_per_loop;
+
+    const int start = blockIdx.x * num_elements_per_SM;
+
+    for (int i = 0; i < num_loops_B; i++) {
+        int index = thread_id + i * num_loops_B + start;
+        for (int j = 0; j < vector_instruction_width; j++) {
+            // x_vec[index + j]
         }
     }
-
-    // __syncthreads();
 
     // // TODO add code here
 
